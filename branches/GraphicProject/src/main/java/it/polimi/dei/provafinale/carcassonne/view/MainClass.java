b33a75cdfd97a95b2fe31d4b437a9993dabeff67
ThinkyClaire <ThinkyClaire@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-12 08:58:42 +0000

    Main con una tessera per verificare la correttezza della stampa.

--- branches/GraphicProject/src/main/java/it/polimi/dei/provafinale/carcassonne/view/MainClass.java
+++ branches/GraphicProject/src/main/java/it/polimi/dei/provafinale/carcassonne/view/MainClass.java
@@ -1,5 +1,7 @@
 package it.polimi.dei.provafinale.carcassonne.view;
 
+import it.polimi.dei.provafinale.carcassonne.model.card.Card;
+
 import javax.swing.JFrame;
 
 public class MainClass {
@@ -9,6 +11,8 @@ public class MainClass {
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		frame.add(new TilePanel());
+		Card c = new Card("N=C S=C O=S E=S NS=1 NE=0 NO=0 OE=0 SE=0 SO=0");
+		
+		frame.add(new TilePanel(c));
 	}
 }
