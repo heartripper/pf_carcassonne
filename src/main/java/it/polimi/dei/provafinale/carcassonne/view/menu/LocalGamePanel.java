28f39ec2c9645efee2d8043c97a150aa5a36306b
Chiara <kia90yaya@hotmail.it>
2012-05-14 16:04:02 +0000

    Ripuliti e riordinati i codici.

--- src/main/java/it/polimi/dei/provafinale/carcassonne/view/menu/LocalGamePanel.java
+++ src/main/java/it/polimi/dei/provafinale/carcassonne/view/menu/LocalGamePanel.java
@@ -16,45 +16,68 @@ import java.awt.Font;
 
 
 public class LocalGamePanel extends JPanel {
+	
 	public LocalGamePanel() {
 		
 		setBackground(new Color(0,0,0,0));
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
 		JPanel titlePanel = new JPanel();
-		add(titlePanel);
 		titlePanel.setLayout(new BorderLayout(0, 0));
+		titlePanel.setBackground(new Color(0,0,0,0));
+		add(titlePanel);
+		
+		//new local game
 		
 		JLabel lblNewLocalGame = new JLabel("NEW LOCAL GAME");
+		lblNewLocalGame.setForeground(Color.WHITE);
 		lblNewLocalGame.setFont(new Font("Papyrus", Font.BOLD, 30));
 		lblNewLocalGame.setHorizontalAlignment(SwingConstants.CENTER);
 		titlePanel.add(lblNewLocalGame, BorderLayout.CENTER);
 		
+		//select players number (Label)
+		
 		JPanel selectPlayersNumberPanel = new JPanel();
+		selectPlayersNumberPanel.setBackground(new Color(0,0,0,0));
 		add(selectPlayersNumberPanel);
 		
 		JLabel lblSelectPlayersNumber = new JLabel("Select players number:");
+		lblSelectPlayersNumber.setForeground(Color.WHITE);
 		lblSelectPlayersNumber.setFont(new Font("Papyrus", Font.PLAIN, 20));
 		selectPlayersNumberPanel.add(lblSelectPlayersNumber);
 		
+		//select players number (ComboBox)
+		
 		String[] data = {"2", "3", "4", "5"};
 		JComboBox comboBox = new JComboBox(data);
+		comboBox.setForeground(Color.BLACK);
+		comboBox.setBackground(Color.WHITE);
 		comboBox.setFont(new Font("Papyrus", Font.PLAIN, 20));
 		selectPlayersNumberPanel.add(comboBox);
 		
+		//play the game
+		
 		JPanel playTheGamePanel = new JPanel();
-		add(playTheGamePanel);
 		playTheGamePanel.setLayout(new BorderLayout(0, 0));
+		playTheGamePanel.setBackground(new Color(0,0,0,0));
+		add(playTheGamePanel);
 		
 		JButton btnPlayTheGame = new JButton("Play the Game");
+		btnPlayTheGame.setForeground(Color.BLACK);
+		btnPlayTheGame.setBackground(Color.WHITE);
 		btnPlayTheGame.setFont(new Font("Papyrus", Font.PLAIN, 23));
 		playTheGamePanel.add(btnPlayTheGame, BorderLayout.CENTER);
 		
+		//back to home
+		
 		JPanel backToHomePanel = new JPanel();
 		backToHomePanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+		backToHomePanel.setBackground(new Color(0,0,0,0));
 		add(backToHomePanel);
 		
 		JButton btnBackToHome = new JButton("Back to Home");
+		btnBackToHome.setForeground(Color.BLACK);
+		btnBackToHome.setBackground(Color.WHITE);
 		btnBackToHome.setFont(new Font("Papyrus", Font.PLAIN, 15));
 		btnBackToHome.addActionListener(new MenuPanelSwitcher("HomePanel"));
 		backToHomePanel.add(btnBackToHome);
