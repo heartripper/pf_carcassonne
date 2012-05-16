465b72f360e84d421f8faabb4c508086f1f5a6cc
dariodotcom <heartripper@users.noreply.github.com>
2012-05-16 20:32:05 +0000

    Updated MainClass to run newer textualInterface

--- src/main/java/it/polimi/dei/provafinale/carcassonne/MainClass.java
+++ src/main/java/it/polimi/dei/provafinale/carcassonne/MainClass.java
@@ -1,12 +1,10 @@
 package it.polimi.dei.provafinale.carcassonne;
 
 import it.polimi.dei.provafinale.carcassonne.model.MatchHandler;
-import it.polimi.dei.provafinale.carcassonne.model.gameinterface.TextualInterface;
+import it.polimi.dei.provafinale.carcassonne.model.textinterface.TextualInterface;
 
 public class MainClass {
 	public static void main(String[] args) {
-//		GameEngine.getInstance().startGame();
-		
 		TextualInterface textIf = new TextualInterface(System.out, System.in);
 		MatchHandler mh = new MatchHandler(textIf);
 		mh.startGame();
