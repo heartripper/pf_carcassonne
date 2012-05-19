5998783d7985ee6294a00647c5ada894a298fd9e
ThinkyClaire <ThinkyClaire@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-19 16:34:11 +0000

    Pannello delle notifiche abbastanza carino.

--- branches/GraphicProject/src/main/java/it/polimi/dei/provafinale/carcassonne/view/game/GamePanel.java
+++ branches/GraphicProject/src/main/java/it/polimi/dei/provafinale/carcassonne/view/game/GamePanel.java
@@ -17,107 +17,102 @@ import javax.swing.JRadioButton;
 import javax.swing.JComboBox;
 import java.awt.Component;
 import javax.swing.Box;
+import javax.swing.SwingConstants;
 
 public class GamePanel extends JPanel {
-	private JTextField xTextField;
-	private JTextField yTextField;
+	private JTextField textField;
+
 	public GamePanel() {
 		setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel gamePanel = new JPanel();
 		add(gamePanel, BorderLayout.CENTER);
-		
+
 		JLabel tilesLabel = new JLabel("");
 
 		JPanel gridPanel = new JPanel();
 		gridPanel.setLayout(new GridLayout());
-		
-		Dimension dim = new Dimension(10,10000);
+
+		Dimension dim = new Dimension(10, 10000);
 		gamePanel.setLayout(new BoxLayout(gamePanel, BoxLayout.X_AXIS));
 		tilesLabel.setSize(dim);
 		tilesLabel.setMaximumSize(dim);
 		tilesLabel.setMinimumSize(dim);
 		tilesLabel.setPreferredSize(dim);
-		
+
 		JPanel tilesPanel = new JPanel();
-		tilesPanel.setBackground(new Color(0,0,0,0));
+		tilesPanel.setBackground(new Color(0, 0, 0, 0));
 		tilesPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setViewportView(tilesPanel);
 		tilesPanel.add(tilesLabel);
 		gamePanel.add(scrollPane);
-		
+
 		JPanel notificationPanel = new JPanel();
 		notificationPanel.setBackground(Color.GRAY);
 		notificationPanel.setPreferredSize(new Dimension(195, 10));
 		add(notificationPanel, BorderLayout.EAST);
-		notificationPanel.setLayout(new BoxLayout(notificationPanel, BoxLayout.Y_AXIS));
-		
+		notificationPanel.setLayout(new BoxLayout(notificationPanel,
+				BoxLayout.Y_AXIS));
+
 		JPanel cardPanel = new JPanel();
 		notificationPanel.add(cardPanel);
-		
+
 		JPanel rotatePanel = new JPanel();
 		notificationPanel.add(rotatePanel);
-		
+
 		JButton btnRotate = new JButton("Rotate");
 		rotatePanel.add(btnRotate);
-		
+
 		JPanel insertCoordinatesPanel = new JPanel();
 		notificationPanel.add(insertCoordinatesPanel);
-		insertCoordinatesPanel.setLayout(new BoxLayout(insertCoordinatesPanel, BoxLayout.Y_AXIS));
+		insertCoordinatesPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JLabel lblInsertCoordinates = new JLabel("Insert coordinates");
-		insertCoordinatesPanel.add(lblInsertCoordinates);
+		JLabel lblInsertCoordinatesxy = new JLabel("Insert coordinates (x,y):");
+		insertCoordinatesPanel.add(lblInsertCoordinatesxy);
 		
-		JPanel xyPanel = new JPanel();
-		insertCoordinatesPanel.add(xyPanel);
-		xyPanel.setLayout(new BoxLayout(xyPanel, BoxLayout.X_AXIS));
-		
-		JLabel lblX = new JLabel("x:");
-		xyPanel.add(lblX);
-		
-		xTextField = new JTextField();
-		xyPanel.add(xTextField);
-		xTextField.setColumns(10);
-		
-		JLabel lblY = new JLabel("y:");
-		xyPanel.add(lblY);
-		
-		yTextField = new JTextField();
-		xyPanel.add(yTextField);
-		yTextField.setColumns(10);
-		
+		textField = new JTextField();
+		insertCoordinatesPanel.add(textField);
+		textField.setColumns(10);
+
 		JPanel followerPanel = new JPanel();
 		notificationPanel.add(followerPanel);
-		
+		followerPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
 		JLabel lblFollower = new JLabel("Follower:");
 		followerPanel.add(lblFollower);
-		
-		String[] followerOptions = {"none", "north", "east", "south", "west"};
+
+		String[] followerOptions = { "none", "north", "east", "south", "west" };
 		JComboBox comboBox = new JComboBox(followerOptions);
 		followerPanel.add(comboBox);
-		
+
 		JPanel putPanel = new JPanel();
 		notificationPanel.add(putPanel);
-		
+
 		JButton btnPut = new JButton("Put!");
 		putPanel.add(btnPut);
 		
+		JPanel takeScreenshotPanel = new JPanel();
+		notificationPanel.add(takeScreenshotPanel);
+		
+		JButton btnTakeAScreenshot = new JButton("Take a ScreenShot");
+		takeScreenshotPanel.add(btnTakeAScreenshot);
+
 		JPanel bottomPanel = new JPanel();
 		add(bottomPanel, BorderLayout.SOUTH);
 		bottomPanel.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel backToHomePanel = new JPanel();
 		bottomPanel.add(backToHomePanel, BorderLayout.EAST);
-		
+
 		JButton btnBackToHome = new JButton("Back to Home");
 		btnBackToHome.setFont(new Font("Papyrus", Font.PLAIN, 15));
 		backToHomePanel.add(btnBackToHome);
-		
+
 		JPanel playerPanel = new JPanel();
 		bottomPanel.add(playerPanel, BorderLayout.WEST);
-		
+
 	}
 
 }
