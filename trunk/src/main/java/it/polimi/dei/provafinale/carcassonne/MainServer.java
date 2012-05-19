521deca87267d2d46a72932293641c98086cdcd8
dariodotcom <dariodotcom@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-19 15:13:47 +0000

    Remove not needed files

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/MainServer.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.dei.provafinale.carcassonne;
-
-import it.polimi.dei.provafinale.carcassonne.model.server.CarcassonneServer;
-
-public class MainServer {
-	public static void main(String[] args) {
-		(new CarcassonneServer(64321)).startRequestMonitor();
-	}
-}
