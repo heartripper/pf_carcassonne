746cc6ceaf2f283063deb693bcd6c5cc6da1088e
dariodotcom <dariodotcom@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-19 15:13:09 +0000

    Move new version into trunk

---
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/gameinterface/MessageType.java
@@ -0,0 +1,5 @@
+package it.polimi.dei.provafinale.carcassonne.model.gameinterface;
+
+public enum MessageType {
+	START, TURN, NEXT, ROTATION, PLACE, FOLLOWER, PASS, UPDATE, INVALID_MOVE, SCORE, END;
+}
