2edfae0fffdecbf74a24adb39e3dff03419121fb
dariodotcom <heartripper@users.noreply.github.com>
2012-05-19 15:13:47 +0000

    Remove not needed files

--- src/main/java/it/polimi/dei/provafinale/carcassonne/MainClient.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.dei.provafinale.carcassonne;
-
-import it.polimi.dei.provafinale.carcassonne.model.client.CarcassonneClient;
-
-public class MainClient {
-	public static void main(String[] args) {
-		(new CarcassonneClient("localhost", 64321)).start();
-	}
-}
