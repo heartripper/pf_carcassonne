19c5eced3d2560bf3a618f64c432d9e55d5a2b79
dariodotcom <dariodotcom@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-22 11:06:07 +0000

    Move ClientServer into trunk

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/gameinterface/MessageType.java
+++
@@ -1,5 +0,0 @@
-package it.polimi.dei.provafinale.carcassonne.model.gameinterface;
-
-public enum MessageType {
-	START, TURN, NEXT, ROTATION, PLACE, FOLLOWER, PASS, UPDATE, INVALID_MOVE, SCORE, END;
-}
