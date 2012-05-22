5d01981f8751da53da61d90c69797e5d370a41a4
Chiara <kia90yaya@hotmail.it>
2012-05-22 12:31:33 +0000

    Update tile representation

--- src/main/java/it/polimi/dei/provafinale/carcassonne/view/game/TilePainter.java
+++ src/main/java/it/polimi/dei/provafinale/carcassonne/view/game/TilePainter.java
@@ -1,9 +1,11 @@
 package it.polimi.dei.provafinale.carcassonne.view.game;
 
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 
@@ -17,7 +19,21 @@ public class TilePainter {
 	private String tilePath = "src/main/resources/card.png";
 	private BufferedImage tileImage;
 	
+	private String cardId = "scr/main/resources/carcassonne.dat";
+	
+	private HashMap<String,Image> cards;
+	
 	private static TilePainter instance = null;
+	
+	private TilePainter() {
+		try {
+			tilePlaceHolder = ImageIO.read(new File(placeHolderPath));
+			tileImage = ImageIO.read(new File(tilePath));
+			
+		} catch (IOException ioe) {
+			System.out.println("Error reading tile images.");
+		}
+	}
 	
 	public static TilePainter getInstance(){
 		if(instance == null)
@@ -26,20 +42,15 @@ public class TilePainter {
 		return instance;
 	}
 	
-	private TilePainter() {
-		try {
-			tilePlaceHolder = ImageIO.read(new File(placeHolderPath));
-			tileImage = ImageIO.read(new File(tilePath));
-		} catch (IOException ioe) {
-			System.out.println("Error reading tile images.");
-		}
-	}
-
 	public void paintTile(String rep, Graphics g, int x, int y) {
 		g.drawImage(tileImage, x, y, TILE_DIM, TILE_DIM, null);
 	}
 
 	public void paintPlaceHolder(Graphics g, int x, int y) {
 		g.drawImage(tilePlaceHolder, x, y, TILE_DIM, TILE_DIM, null);
+	}
+	
+	private void initializeCards (String cardId){
+		
 	}
 }
