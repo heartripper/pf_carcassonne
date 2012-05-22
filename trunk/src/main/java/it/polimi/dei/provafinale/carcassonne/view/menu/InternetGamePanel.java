ad71abcde280108e43aa34f00a829bc17ff4384f
ThinkyClaire <ThinkyClaire@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-22 13:48:01 +0000

    start implementing controller

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/view/menu/InternetGamePanel.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/view/menu/InternetGamePanel.java
@@ -1,105 +1,140 @@
 package it.polimi.dei.provafinale.carcassonne.view.menu;
 
-import it.polimi.dei.provafinale.carcassonne.controller.MenuPanelSwitcher;
-
-import javax.swing.JPanel;
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import java.awt.Component;
-import javax.swing.Box;
-import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
-import java.awt.GridLayout;
-import java.awt.Dimension;
-import java.awt.CardLayout;
-import java.awt.Image;
 import java.awt.LayoutManager;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import it.polimi.dei.provafinale.carcassonne.controller.MenuPanelSwitcher;
+import it.polimi.dei.provafinale.carcassonne.controller.client.StartInternetGame;
 
 public class InternetGamePanel extends JPanel {
+
+	private static final long serialVersionUID = 8480541024375120364L;
+
+	private JLabel messageLabel;
+	private JTextField serverIPField;
+	private JTextField serverPortField;
 	
-	private JTextField textField;
-	
-	private Image background;
-	
-	public InternetGamePanel(Image background) {
+	private BufferedImage background;
+
+	public InternetGamePanel(BufferedImage background) {
 		this.background = background;
-		
+
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		
+
 		Component verticalGlue01 = Box.createVerticalGlue();
 		add(verticalGlue01);
-		
+
 		JPanel titlePanel = new JPanel();
-		titlePanel.setBackground(new Color(0,0,0,0));
+		titlePanel.setBackground(new Color(0, 0, 0, 0));
 		add(titlePanel);
 		titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.X_AXIS));
-		
+
 		JLabel lblNewInternetGame = new JLabel("NEW INTERNET GAME");
 		titlePanel.add(lblNewInternetGame);
-		
+
 		Component verticalStrut_1 = Box.createVerticalStrut(20);
 		add(verticalStrut_1);
-		
-		JPanel insertServerIpAddressPanel = new JPanel();
-		insertServerIpAddressPanel.setBackground(new Color(0,0,0,0));
-		insertServerIpAddressPanel.setPreferredSize(new Dimension(10, 0));
-		add(insertServerIpAddressPanel);
-		LayoutManager layout = new FlowLayout();
-		insertServerIpAddressPanel.setLayout(layout);
-		
+
+		JPanel serverInfoPanel = new JPanel();
+		serverInfoPanel.setBackground(new Color(0, 0, 0, 0));
+		serverInfoPanel.setPreferredSize(new Dimension(10, 0));
+		add(serverInfoPanel);
+		BoxLayout boxLayout = new BoxLayout(serverInfoPanel, BoxLayout.Y_AXIS);
+		serverInfoPanel.setLayout(boxLayout);
+
 		Component horizontalGlue01 = Box.createHorizontalGlue();
-		insertServerIpAddressPanel.add(horizontalGlue01);
-		
+		serverInfoPanel.add(horizontalGlue01);
+
+		JPanel IPPanel = new JPanel();
+		IPPanel.setBackground(new Color(0,0,0,0));
 		JLabel lblInsertServerIp = new JLabel("Insert server IP address:");
-		insertServerIpAddressPanel.add(lblInsertServerIp);
-		
-		textField = new JTextField();
-		insertServerIpAddressPanel.add(textField);
-		textField.setColumns(10);
-		
+		IPPanel.add(lblInsertServerIp);
+		serverIPField = new JTextField();
+		IPPanel.add(serverIPField);
+		serverIPField.setColumns(10);
+
+		JPanel portPanel = new JPanel();
+		portPanel.setBackground(new Color(0,0,0,0));
+		JLabel lblInsertPort = new JLabel("Insert server port:");
+		portPanel.add(lblInsertPort);
+		serverPortField = new JTextField();
+		portPanel.add(serverPortField);
+		serverPortField.setColumns(10);
+
+		serverInfoPanel.add(IPPanel);
+		serverInfoPanel.add(portPanel);
+
 		Component horizontalGlue02 = Box.createHorizontalGlue();
-		insertServerIpAddressPanel.add(horizontalGlue02);
-		
+		serverInfoPanel.add(horizontalGlue02);
+
 		Component verticalStrut = Box.createVerticalStrut(20);
 		add(verticalStrut);
-		
+
 		JPanel joinGamePanel = new JPanel();
-		joinGamePanel.setBackground(new Color(0,0,0,0));
+		joinGamePanel.setBackground(new Color(0, 0, 0, 0));
 		add(joinGamePanel);
 		joinGamePanel.setLayout(new BoxLayout(joinGamePanel, BoxLayout.X_AXIS));
-		
+
 		Component horizontalGlue03 = Box.createHorizontalGlue();
 		joinGamePanel.add(horizontalGlue03);
-		
+
 		JButton btnJoinGame = new JButton("Join Game!");
 		joinGamePanel.add(btnJoinGame);
-		
+		btnJoinGame.addActionListener(new StartInternetGame(this));
+
 		Component horizontalGlue04 = Box.createHorizontalGlue();
 		joinGamePanel.add(horizontalGlue04);
-		
+
+		JPanel messagePanel = new JPanel();
+		messagePanel.setBackground(new Color(0, 0, 0, 0));
+		add(messagePanel);
+
+		messageLabel = new JLabel();
+		messagePanel.add(messageLabel);
+
 		Component verticalGlue02 = Box.createVerticalGlue();
 		add(verticalGlue02);
-		
+
 		JPanel backToHomePanel = new JPanel();
-		backToHomePanel.setBackground(new Color(0,0,0,0));
+		backToHomePanel.setBackground(new Color(0, 0, 0, 0));
 		add(backToHomePanel);
-		backToHomePanel.setLayout(new BoxLayout(backToHomePanel, BoxLayout.X_AXIS));
-		
+		backToHomePanel.setLayout(new BoxLayout(backToHomePanel,
+				BoxLayout.X_AXIS));
+
 		JButton btnBackToHome = new JButton("Back to home");
 		backToHomePanel.add(btnBackToHome);
 		ActionListener home = new MenuPanelSwitcher(MenuPanel.HOMEPANEL);
 		btnBackToHome.addActionListener(home);
 	}
-	
+
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		int width = getWidth();
-		g.drawImage(background, (width-778)/2, 0, null);
+		g.drawImage(background, (width - 778) / 2, 0, null);
 	}
-
+	
+	public JLabel getMessageLabel(){
+		return messageLabel;
+	}
+	
+	public String getPortFieldValue(){
+		return serverPortField.getText();
+	}
+	
+	public String getIPFieldValue(){
+		return serverIPField.getText();
+	}
 }
