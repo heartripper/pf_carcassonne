284cae5970db93e26ba490466e6e99e6c1b5e02c
dariodotcom <dariodotcom@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-23 17:21:14 +0000

    Fixed bug in messages handling

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/gameinterface/Message.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/gameinterface/Message.java
@@ -1,25 +1,54 @@
 package it.polimi.dei.provafinale.carcassonne.model.gameinterface;
 
+/**
+ * Class representing messages that are exchanged between MatchHandler and
+ * player clients.
+ * */
 public class Message {
 	public final MessageType type;
 	public final String payload;
 
+	/**
+	 * Creates a new Message with give type and payload.
+	 * 
+	 * @param type
+	 *            - a MessageType
+	 * @param payload
+	 *            - a string containing the message payload
+	 * */
 	public Message(MessageType type, String payload) {
 		this.type = type;
 		this.payload = payload;
 	}
 
+	/**
+	 * Gives the protocol message represented by this Message.
+	 * 
+	 * @return a String containing the protocol message.
+	 * */
 	public String toProtocolMessage() {
 		String msg = type.toString();
-		if (payload != null){
+		if (payload != null) {
 			msg += (": " + payload);
 		}
 		return msg;
 	}
 
+	/**
+	 * Given a protocol message, creates a Message instance representing that
+	 * message.
+	 * 
+	 * @param msg
+	 *            - a string containing the protocol message
+	 * @return a Message representation of protocol message.
+	 */
 	public static Message createFromProtocolMsg(String msg) {
 		String[] split = msg.split(":");
-		MessageType type = MessageType.getTypeFor(split[0]);
-		return new Message(type, split[1].trim());
+		MessageType type = MessageType.getTypeFor(split[0].trim());
+		String payload = null;
+		if (split.length == 2) {
+			payload = split[1].trim();
+		}
+		return new Message(type, payload);
 	}
 }
--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/gameinterface/MessageType.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/gameinterface/MessageType.java
@@ -1,10 +1,14 @@
 package it.polimi.dei.provafinale.carcassonne.model.gameinterface;
 
+/**
+ * Enum containing all possible types of messages used in communications by
+ * match handler and players
+ * */
 public enum MessageType {
 	START("start"), TURN("turn"), NEXT("next"), ROTATE("rotate"), ROTATED(
-			"rotated"), PLACE("update"), FOLLOWER("follower"), PASS("pass"), UPDATE(
-			"update"), INVALID_MOVE("move not valid"), SCORE("score"), END(
-			"end"), LOCK("lock"), UNLOCK("unlock"), LEAVE("leave");
+			"rotated"), PLACE("place"), FOLLOWER("tile"), PASS("pass"), UPDATE(
+			"update"), UPDATE_SINGLE("update"), INVALID_MOVE("move not valid"), SCORE(
+			"score"), END("end"), LOCK("lock"), UNLOCK("unlock"), LEAVE("leave");
 
 	private String protocolMessage;
 
@@ -17,9 +21,17 @@ public enum MessageType {
 		return protocolMessage;
 	}
 
+	/**
+	 * Gives the MessageType corresponding to the given protocol message header.
+	 * 
+	 * @param type
+	 *            - a String containing the protocol message header
+	 * @return the MessageType corresponding to given message header if exists,
+	 *         null otherwise.
+	 * */
 	public static MessageType getTypeFor(String type) {
 		for (MessageType t : MessageType.values())
-			if (t.protocolMessage.equals(type)){
+			if (t.protocolMessage.equals(type)) {
 				return t;
 			}
 		return null;
