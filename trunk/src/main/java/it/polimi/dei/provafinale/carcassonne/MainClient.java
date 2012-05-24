f99480775eae6881ea20cbb579a134b661fe47cc
dariodotcom <dariodotcom@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-24 22:15:31 +0000

    Use new TextualViewInterface for textual client.

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/MainClient.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/MainClient.java
@@ -1,9 +1,23 @@
 package it.polimi.dei.provafinale.carcassonne;
 
-import it.polimi.dei.provafinale.carcassonne.controller.client.CarcassonneClient;
+import it.polimi.dei.provafinale.carcassonne.controller.client.ClientInterface;
+import it.polimi.dei.provafinale.carcassonne.controller.client.ClientSocketInterface;
+import it.polimi.dei.provafinale.carcassonne.controller.client.ConnectionLostException;
+import it.polimi.dei.provafinale.carcassonne.controller.client.MatchController;
+import it.polimi.dei.provafinale.carcassonne.view.viewInterface.TextualViewInterface;
+import it.polimi.dei.provafinale.carcassonne.view.viewInterface.ViewInterface;
 
 public class MainClient {
 	public static void main(String[] args) {
-		(new CarcassonneClient("localhost", 12345)).start();
+		ClientInterface ci = new ClientSocketInterface("localhost", 12345);
+		try{
+			ci.connect();
+		}catch(ConnectionLostException e){
+			System.out.println("Error connecting to server.");
+			return;
+		}
+		
+		ViewInterface vi = new TextualViewInterface();
+		MatchController.startNewMatchController(ci, vi);
 	}
 }
