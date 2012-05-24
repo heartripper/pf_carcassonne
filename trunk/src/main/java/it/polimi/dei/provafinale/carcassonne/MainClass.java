ca83d2372e326429838473857adb8f785227911b
dariodotcom <dariodotcom@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-24 20:29:14 +0000

    Removed unused imports

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/MainClass.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/MainClass.java
@@ -1,13 +1,6 @@
 package it.polimi.dei.provafinale.carcassonne;
 
 import it.polimi.dei.provafinale.carcassonne.model.MatchHandler;
-import it.polimi.dei.provafinale.carcassonne.model.gamelogic.Coord;
-import it.polimi.dei.provafinale.carcassonne.model.gamelogic.Match;
-import it.polimi.dei.provafinale.carcassonne.model.gamelogic.card.Card;
-import it.polimi.dei.provafinale.carcassonne.model.gamelogic.card.SidePosition;
-import it.polimi.dei.provafinale.carcassonne.model.gamelogic.card.TileGrid;
-import it.polimi.dei.provafinale.carcassonne.model.gamelogic.entity.Entity;
-import it.polimi.dei.provafinale.carcassonne.model.gamelogic.player.PlayerColor;
 import it.polimi.dei.provafinale.carcassonne.model.textinterface.TextualInterface;
 
 public class MainClass {
