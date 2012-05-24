f214320a5acfa7e077547e10b7c11f0a2db098e0
dariodotcom <dariodotcom@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-24 07:47:00 +0000

    Fixed problems in Card

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/gamelogic/card/SideConnection.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/gamelogic/card/SideConnection.java
@@ -1,12 +1,28 @@
 package it.polimi.dei.provafinale.carcassonne.model.gamelogic.card;
 
+/**
+ * Class to represent a connection between two sides of a tile. It represents
+ * connections between Sides and not between sidewPositions so it's easier to
+ * perform card rotation.
+ * */
 public class SideConnection {
 	private Side side1;
 	private Side side2;
 
+	/**
+	 * Constructs a SideConnection instance given the two sides that are
+	 * connected.
+	 * @param side1 - a Side
+	 * @param side2 - a Side
+	 * */
 	public SideConnection(Side side1, Side side2) {
 		this.side1 = side1;
 		this.side2 = side2;
+	}
+
+	@Override
+	public int hashCode() {
+		return side1.hashCode() + side2.hashCode();
 	}
 
 	@Override
