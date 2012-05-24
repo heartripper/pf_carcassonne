76ecf7e37cc6e3537587c26811a8befa35180e32
dariodotcom <heartripper@users.noreply.github.com>
2012-05-24 20:13:31 +0000

    Skip close alert when in debug

--- src/main/java/it/polimi/dei/provafinale/carcassonne/controller/WindowClose.java
+++ src/main/java/it/polimi/dei/provafinale/carcassonne/controller/WindowClose.java
@@ -1,5 +1,6 @@
 package it.polimi.dei.provafinale.carcassonne.controller;
 
+import it.polimi.dei.provafinale.carcassonne.Constants;
 import it.polimi.dei.provafinale.carcassonne.view.CarcassonneFrame;
 
 import java.awt.event.WindowAdapter;
@@ -7,21 +8,28 @@ import java.awt.event.WindowEvent;
 
 import javax.swing.JOptionPane;
 
-public class WindowClose extends WindowAdapter{
+public class WindowClose extends WindowAdapter {
 
-	public void windowClosing(WindowEvent e){
-		
+	public void windowClosing(WindowEvent e) {
+
 		CarcassonneFrame frame = (CarcassonneFrame) e.getWindow();
-		int ans = JOptionPane.showConfirmDialog(null, "Are you sure you want to close this window?","Attention!", JOptionPane.YES_NO_OPTION);
-		
-		if(ans == JOptionPane.YES_OPTION){
-			exit(frame);
-			
+
+		if (!Constants.ASK_ON_CLOSE) {
+			close(frame);
+		} else {
+			int ans = JOptionPane.showConfirmDialog(null,
+					"Are you sure you want to close this window?",
+					"Attention!", JOptionPane.YES_NO_OPTION);
+
+			if (ans == JOptionPane.YES_OPTION) {
+				close(frame);
+			}
 		}
 	}
-	
-	static void exit (CarcassonneFrame frame){
+
+	public void close(CarcassonneFrame frame) {
 		frame.dispose();
+		System.exit(0);
 	}
-	
+
 }
