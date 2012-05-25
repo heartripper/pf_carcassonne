838e9ea19e6a83a5566c089e27ca6c1efd8ae9ec
dariodotcom <dariodotcom@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-25 19:13:09 +0000

    Listener to start new local game

---
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/controller/client/StartLocalGame.java
@@ -0,0 +1,29 @@
+package it.polimi.dei.provafinale.carcassonne.controller.client;
+
+import it.polimi.dei.provafinale.carcassonne.model.MatchHandler;
+import it.polimi.dei.provafinale.carcassonne.view.viewInterface.GUIViewInterface;
+import it.polimi.dei.provafinale.carcassonne.view.viewInterface.ViewInterface;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class StartLocalGame implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// Create duplex interface for Client - Match controller communication
+		ClientLocalInterface cli = new ClientLocalInterface(2);
+
+		// Start match handler
+		MatchHandler mh = new MatchHandler(cli);
+		Thread th = new Thread(mh);
+		th.start();
+
+		// Create view interface
+		ViewInterface vi = new GUIViewInterface();
+
+		// Create client controller
+		MatchController.startNewMatchController(cli, vi);
+	}
+
+}
