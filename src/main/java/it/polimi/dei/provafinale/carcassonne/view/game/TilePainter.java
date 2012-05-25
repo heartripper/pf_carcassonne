781436c74302befb8d3bdaa4d1a7ae882addbe0b
Chiara <kia90yaya@hotmail.it>
2012-05-25 12:33:37 +0000

    Aggiunto disegna segnalini.

--- src/main/java/it/polimi/dei/provafinale/carcassonne/view/game/TilePainter.java
+++ src/main/java/it/polimi/dei/provafinale/carcassonne/view/game/TilePainter.java
@@ -1,10 +1,16 @@
 package it.polimi.dei.provafinale.carcassonne.view.game;
 
+import it.polimi.dei.provafinale.carcassonne.model.gamelogic.Coord;
 import it.polimi.dei.provafinale.carcassonne.model.gamelogic.card.Card;
+import it.polimi.dei.provafinale.carcassonne.model.gamelogic.card.SidePosition;
+import it.polimi.dei.provafinale.carcassonne.model.gamelogic.player.PlayerColor;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
@@ -13,6 +19,8 @@ import java.io.IOException;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.imageio.ImageIO;
 
@@ -23,6 +31,8 @@ public class TilePainter {
 
 	private BufferedImage tilePlaceHolder;
 	private String placeHolderPath = "src/main/resources/placeholder.png";
+
+	private Pattern followerPattern;
 
 	private Map<String, BufferedImage> imgLib;
 
@@ -49,16 +59,24 @@ public class TilePainter {
 					imgLib.put(line, currentImg);
 				}
 			}
-			
+
 			tilePlaceHolder = ImageIO.read(new File(placeHolderPath));
 		} catch (IOException ioe) {
 			System.out.println("Error reading tile images.");
 		}
+
+		String followerRegExp = "[NESW]=.-[RBVGN]";
+		followerPattern = Pattern.compile(followerRegExp);
 	}
 
 	public void paintTile(String rep, Graphics g, int x, int y) {
-		BufferedImage img = getImage(rep);
+		// Remove followers
+		String base = rep.replaceAll("-.", "");
+		BufferedImage img = getImage(base);
 		g.drawImage(img, x, y, tileDim, tileDim, null);
+		if (rep.indexOf('-') != -1) {
+
+		}
 	}
 
 	public void paintPlaceHolder(Graphics g, int x, int y) {
@@ -96,7 +114,7 @@ public class TilePainter {
 						+ ".");
 				return null;
 			}
-			
+
 			BufferedImage baseImage = imgLib.get(c.toString());
 			BufferedImage image = rotateImage(baseImage, rotCount);
 			imgLib.put(rep, image);
@@ -107,14 +125,48 @@ public class TilePainter {
 	private BufferedImage rotateImage(BufferedImage baseImage, int rotCount) {
 		// creating the AffineTransform instance
 		AffineTransform affineTransform = new AffineTransform();
-		/*Rotate the image.*/
+		/* Rotate the image. */
 		affineTransform.rotate(Math.toRadians(90 * rotCount));
-		/*Draw the image using the AffineTransform.*/
+		/* Draw the image using the AffineTransform. */
 		BufferedImage rotateImage = new BufferedImage(tileDim, tileDim,
 				BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g = rotateImage.createGraphics();
 		g.drawImage(baseImage, affineTransform, null);
 		g.dispose();
 		return rotateImage;
+	}
+
+	private void paintFollower(String rep, Graphics g, int x, int y) {
+		Matcher m = followerPattern.matcher(rep);
+		char posChar = rep.charAt(m.start());
+		char colChar = rep.charAt(m.end());
+		PlayerColor color = PlayerColor.valueOf(String.valueOf(posChar));
+		SidePosition pos = SidePosition.valueOf(String.valueOf(colChar));
+		
+		g.setColor(color.getColor());
+		
+		int smallDist = tileDim / 16;
+		int mediumDist = tileDim / 8;
+		Point p;
+		switch (pos){
+		case N: 
+			p = new Point(7*smallDist, smallDist);
+			break;
+		case S:
+			p = new Point(7*smallDist, 13*smallDist);
+			break;
+		case W:
+			p = new Point(smallDist, 7*smallDist);
+			break;
+		case E:
+			p = new Point(13*smallDist, 7*smallDist);
+			break;
+		default: 
+			p = null;
+			break;
+		}
+		
+		g.drawOval(p.x, p.y, mediumDist, mediumDist);
+		
 	}
 }
