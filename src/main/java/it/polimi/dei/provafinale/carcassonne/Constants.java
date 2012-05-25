cb8f701f48db852e7c578e5bdfb188dadd71cfb9
dariodotcom <heartripper@users.noreply.github.com>
2012-05-25 04:43:46 +0000

    Modifiers in right order

--- src/main/java/it/polimi/dei/provafinale/carcassonne/Constants.java
+++ src/main/java/it/polimi/dei/provafinale/carcassonne/Constants.java
@@ -2,19 +2,19 @@ package it.polimi.dei.provafinale.carcassonne;
 
 public abstract class Constants {
 
-	public final static int SIDES_NUMBER = 4;
+	public static final int SIDES_NUMBER = 4;
 
-	public final static int TILE_PIXEL_DIMENSION = 125;
+	public static final int TILE_PIXEL_DIMENSION = 125;
 
-	public final static boolean ASK_ON_CLOSE = false;
+	public static final boolean ASK_ON_CLOSE = false;
 	
-	public final static boolean DEBUG_MODE = true;
-	public final static String DEBUG_ADDR = "localhost";
-	public final static int DEBUG_PORT = 12345;
+	public static final boolean DEBUG_MODE = true;
+	public static final String DEBUG_ADDR = "localhost";
+	public static final int DEBUG_PORT = 12345;
 
 	//Internet game panel.
-	public final static String MATCH_IS_STARTING = "Connected to server. Waiting for match to begin.";
-	public final static String FIELDS_ERROR = "Please fill in all fields.";
-	public final static String PORT_ERROR = "Please insert a valid port value.";
-	public final static String CONNECTION_ERROR = "Connection to server failed.";
+	public static final String MATCH_IS_STARTING = "Connected to server. Waiting for match to begin.";
+	public static final String FIELDS_ERROR = "Please fill in all fields.";
+	public static final String PORT_ERROR = "Please insert a valid port value.";
+	public static final String CONNECTION_ERROR = "Connection to server failed.";
 }
