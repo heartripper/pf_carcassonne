ded18573d5f89eff6e4a42a4f61ccd259191edb4
ThinkyClaire <ThinkyClaire@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-25 18:37:32 +0000

    Added Javadoc.

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/view/CarcassonneFrame.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/view/CarcassonneFrame.java
@@ -9,57 +9,96 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 
-public class CarcassonneFrame extends JFrame{
-	
+public class CarcassonneFrame extends JFrame {
+
 	private static final long serialVersionUID = -7524847858570259831L;
+
 	public static final String MENUPANEL = "menupanel";
 	public static final String GAMEPANEL = "gamepanel";
-	
+
+	private CardLayout mainLayout;
 	private MenuPanel menuPanel;
 	private JPanel gamePanelContainer;
 	private GamePanel gamePanel;
-	private CardLayout mainLayout;
-	
+
+	/**
+	 * CarcassonneFrame constructor. Creates a new instance of class
+	 * CarcassonneFrame.
+	 */
 	public CarcassonneFrame() {
-		
-		//TODO AGGIUNGERE ICONA GIOCO NEL PROFILO DELLA FINESTRA
-		
+
+		// TODO AGGIUNGERE ICONA GIOCO NEL PROFILO DELLA FINESTRA
+
+		/* Superclass constructor invocation. */
 		super("Carcassonne");
-		
+
+		/*
+		 * Adding option "Are you sure you want to close this window?" to
+		 * CarcassonneFrame.
+		 */
 		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-        addWindowListener(new WindowClose());
-        
+		addWindowListener(new WindowClose());
+
 		setVisible(true);
 		setSize(600, 700);
-		
-		mainLayout = new CardLayout(0,0);
+
+		/* Setting layout. */
+		mainLayout = new CardLayout(0, 0);
 		setLayout(mainLayout);
-		
+
+		/* Setting menuPanel. */
 		menuPanel = new MenuPanel();
 		add(menuPanel, MENUPANEL);
 		menuPanel.setVisible(true);
-		
+
+		/* Setting gamePanelContainer. */
 		gamePanelContainer = new JPanel();
 		add(gamePanelContainer, GAMEPANEL);
 		gamePanelContainer.setLayout(new BorderLayout());
-		
+
 		repaint();
 	}
 
+	/**
+	 * Manages the change of mainPanel (eg. menu or game view) in
+	 * CarcassonneFrame.
+	 * 
+	 * @param destination
+	 *            a String representing the name of the panel we want to put in
+	 *            mainLayout.
+	 */
+	public void changeMainPanel(String destination) {
+		mainLayout.show(this.getContentPane(), destination);
+	}
+
+	/**
+	 * Manages the change of menuPanel (eg. from HomePanel to LocalGamePanel,
+	 * from HomePanel to InternetGamePanel) in CarcassonneFrame.
+	 * 
+	 * @param destination
+	 *            a String representing the name of the panel we want to put in
+	 *            menuPanel.
+	 */
 	public void changeMenuPanel(String destination) {
 		menuPanel.changeMenuPanel(destination);
 	}
-	
-	public void setGamePanel(GamePanel gamePanel){
-		if(this.gamePanel == null){
+
+	/**
+	 * Sets a new instance of GamePanel if it doesn't exist.
+	 * 
+	 * @param gamePanel
+	 *            an instance of class GamePanel.
+	 */
+	public void setGamePanel(GamePanel gamePanel) {
+		/* There are no instances of GamePanel. */
+		if (this.gamePanel == null) {
 			this.gamePanel = gamePanel;
 			gamePanelContainer.add(gamePanel, BorderLayout.CENTER);
-		}else{
+		}
+		/* An instance of GamePanel alredy exists. */
+		else {
 			return;
 		}
 	}
-	
-	public void changeMainPanel(String destination){
-		mainLayout.show(this.getContentPane(), destination);
-	}
+
 }
--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/view/ViewManager.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/view/ViewManager.java
@@ -2,36 +2,59 @@ package it.polimi.dei.provafinale.carcassonne.view;
 
 import java.awt.Graphics;
 
-public class ViewManager{
-	
+public class ViewManager {
+
 	private static ViewManager instance = null;
 	private CarcassonneFrame frame;
-	
-	private ViewManager(){
+
+	private ViewManager() {
 		frame = new CarcassonneFrame();
 	}
 
-	public static ViewManager getInstance(){
-		if (instance == null){
+	/**
+	 * Gives an instance of ViewManager. If it doesn't exist, the method creates
+	 * a new one.
+	 * 
+	 * @return an instance of ViewManager.
+	 */
+	public static ViewManager getInstance() {
+
+		/* There are no instances of ViewManager. */
+		if (instance == null) {
 			instance = new ViewManager();
 		}
-		
+
 		return instance;
 	}
-	
-	public void changeMenuPanel(String panelName){
+
+	/**
+	 * Manages the change of menuPanel (eg. from HomePanel to LocalGamePanel,
+	 * from HomePanel to InternetGamePanel) in ViewManager.
+	 * 
+	 * @param panelNamea
+	 *            String representing the name of the panel we want to put in
+	 *            frame.
+	 */
+	public void changeMenuPanel(String panelName) {
 		frame.changeMenuPanel(panelName);
 	}
-	
-	public CarcassonneFrame getFrame(){
+
+	/**
+	 * 
+	 * @return the current instance of frame.
+	 */
+	public CarcassonneFrame getFrame() {
 		return frame;
 	}
-	
-	public void updateView(){
+
+	/**
+	 * Updates the current view of frame.
+	 */
+	public void updateView() {
 		Graphics g = frame.getGraphics();
-		if(g != null){
+		if (g != null) {
 			frame.paint(g);
 		}
 	}
-	
+
 }
