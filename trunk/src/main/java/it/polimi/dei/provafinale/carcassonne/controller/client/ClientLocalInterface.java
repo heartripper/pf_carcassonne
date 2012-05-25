3752d8869b3edf04b702895e8139f2f537be3518
dariodotcom <dariodotcom@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-25 19:12:42 +0000

    Use messagebuffer for local interface

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/controller/client/ClientLocalInterface.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/controller/client/ClientLocalInterface.java
@@ -2,18 +2,21 @@ package it.polimi.dei.provafinale.carcassonne.controller.client;
 
 import it.polimi.dei.provafinale.carcassonne.model.gameinterface.GameInterface;
 import it.polimi.dei.provafinale.carcassonne.model.gameinterface.Message;
+import it.polimi.dei.provafinale.carcassonne.model.gameinterface.MessageBuffer;
 import it.polimi.dei.provafinale.carcassonne.model.gameinterface.MessageType;
 import it.polimi.dei.provafinale.carcassonne.model.gamelogic.player.PlayerColor;
 import it.polimi.dei.provafinale.carcassonne.model.server.PlayersDisconnectedException;
 
 public class ClientLocalInterface implements GameInterface, ClientInterface {
 
-	private Message clientMessage;
-	private Message modelMessage;
+	private MessageBuffer modelBuffer;
+	private MessageBuffer clientBuffer;
 	private int playerNumber;
 
 	public ClientLocalInterface(int playerNumber) {
 		this.playerNumber = playerNumber;
+		this.modelBuffer = new MessageBuffer();
+		this.clientBuffer = new MessageBuffer();
 	}
 
 	// Client interface
@@ -24,35 +27,17 @@ public class ClientLocalInterface implements GameInterface, ClientInterface {
 	}
 
 	@Override
-	public synchronized void sendMessage(Message msg)
-			throws ConnectionLostException {
-		while(clientMessage != null){
-			try{
-				wait();
-			}catch(InterruptedException e){}
-		}
-		clientMessage = msg;
-		notifyAll();
-//		System.out.println("Client says: " + msg.toProtocolMessage());
+	public void sendMessage(Message msg) {
+		clientBuffer.write(msg);
 	}
 
 	@Override
-	public synchronized Message readMessage() throws ConnectionLostException {
-		while(modelMessage == null){
-			try{
-				wait();
-			}catch(InterruptedException e){}
-		}
-		Message msg = modelMessage;
-		modelMessage = null;
-		notifyAll();
-//		System.out.println("Client reads: " + msg);
-		return msg;
+	public Message readMessage() {
+		return modelBuffer.read();
 	}
 
 	@Override
-	public void reconnect(String matchName, PlayerColor color)
-			throws ConnectionLostException {
+	public void reconnect(String matchName, PlayerColor color) {
 		// Nothing to do here in local game.
 		return;
 	}
@@ -64,51 +49,29 @@ public class ClientLocalInterface implements GameInterface, ClientInterface {
 	}
 
 	@Override
-	public synchronized Message readFromPlayer(PlayerColor player)
-			throws PlayersDisconnectedException {
-		while(clientMessage == null){
-			try{
-				wait();
-			}catch(InterruptedException e){}
-		}
-		Message msg = clientMessage;
-		clientMessage = null;
-		notifyAll();
-//		System.out.println("Server reads: " + msg);
-		return msg;
+	public Message readFromPlayer(PlayerColor player) {
+		return clientBuffer.read();
 	}
 
 	@Override
-	public synchronized void sendPlayer(PlayerColor color, Message msg)
-			throws PlayersDisconnectedException {
+	public void sendPlayer(PlayerColor color, Message msg) {
 		if (msg.type == MessageType.UPDATE_SINGLE) {
 			msg = new Message(MessageType.UPDATE, msg.payload);
 		}
-		while(modelMessage != null){
-			try{
-				wait();
-			}catch(InterruptedException e){}
-		}
-		modelMessage = msg;
-		notifyAll();
-//		System.out.println("Server says: " + msg.toProtocolMessage());
+		modelBuffer.write(msg);
 	}
 
 	@Override
-	public synchronized void sendAllPlayer(Message msg) throws PlayersDisconnectedException {
+	public void sendAllPlayer(Message msg)
+			throws PlayersDisconnectedException {
 		if (msg.type == MessageType.START) {
 			String tile = msg.payload;
 			String payload = String.format("%s, %s, %s, %s", tile, "localgame",
 					"null", playerNumber);
 			msg = new Message(MessageType.START, payload);
 		}
-		while(modelMessage != null){
-			try{
-				wait();
-			}catch(InterruptedException e){}
-		}
-		modelMessage = msg;
-		notifyAll();
-//		System.out.println("Server says to all: " + msg.toProtocolMessage());
+
+		modelBuffer.write(msg);
 	}
+
 }
