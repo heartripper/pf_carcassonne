679b79cbcc56fe782925f438903a1845a661564f
Chiara <kia90yaya@hotmail.it>
2012-05-25 13:42:25 +0000

    Aggiungi costante per giocare con un numero ridotto di tessere (per debug)

--- src/main/java/it/polimi/dei/provafinale/carcassonne/Constants.java
+++ src/main/java/it/polimi/dei/provafinale/carcassonne/Constants.java
@@ -8,10 +8,15 @@ public abstract class Constants {
 
 	public static final boolean ASK_ON_CLOSE = false;
 	
+	public static final String TILES_PATH = "src/main/resources/carcassonne.dat";
+	public static final String LESS_TILES_PATH = "src/main/resources/carcassonne_mini.dat";
+	
+	//Debug constants
 	public static final boolean DEBUG_MODE = true;
 	public static final String DEBUG_ADDR = "localhost";
 	public static final int DEBUG_PORT = 12345;
-
+	public static final boolean USE_FEW_TILES = false;
+	
 	//Internet game panel.
 	public static final String MATCH_IS_STARTING = "Connected to server. Waiting for match to begin.";
 	public static final String FIELDS_ERROR = "Please fill in all fields.";
