10d4bbbcf24a96234f6ed279f2ba4977db47e184
ThinkyClaire <ThinkyClaire@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-25 21:11:34 +0000

    Added Javadoc.

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/controller/client/ConnectionLostException.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/controller/client/ConnectionLostException.java
@@ -1,14 +1,24 @@
 package it.polimi.dei.provafinale.carcassonne.controller.client;
 
-public class ConnectionLostException extends Exception{
+public class ConnectionLostException extends Exception {
 
 	private static final long serialVersionUID = 7560807455345741619L;
 
-	public ConnectionLostException(){
+	/**
+	 * Manages the exception of type ConnectionLostException.
+	 */
+	public ConnectionLostException() {
 		super();
 	}
-	
-	public ConnectionLostException(String s){
+
+	/**
+	 * Manages the exception of type ConnectionLostException.
+	 * 
+	 * @param s
+	 *            a String that indicates an error.
+	 */
+	public ConnectionLostException(String s) {
 		super(s);
 	}
+	
 }
