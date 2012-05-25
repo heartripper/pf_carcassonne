d63f57ea11a96ea1ea567ea986b939f0287d9d51
dariodotcom <heartripper@users.noreply.github.com>
2012-05-25 16:31:52 +0000

    Implemented ClientLocalInterface to directly link Model MatchHandler and Client Match Controller

---
+++ src/main/java/it/polimi/dei/provafinale/carcassonne/StartLocalGame.java
@@ -0,0 +1,22 @@
+package it.polimi.dei.provafinale.carcassonne;
+
+import it.polimi.dei.provafinale.carcassonne.controller.client.ClientLocalInterface;
+import it.polimi.dei.provafinale.carcassonne.controller.client.MatchController;
+import it.polimi.dei.provafinale.carcassonne.model.MatchHandler;
+import it.polimi.dei.provafinale.carcassonne.view.viewInterface.TextualViewInterface;
+
+public class StartLocalGame {
+	public static void main(String[] args) {
+		ClientLocalInterface cli = new ClientLocalInterface(2);
+		//Create match handler
+		MatchHandler mh = new MatchHandler(cli);
+		Thread th = new Thread(mh);
+		th.start();
+		
+		//Create textual view
+		TextualViewInterface vi = new TextualViewInterface();
+		
+		//Create client controller
+		MatchController.startNewMatchController(cli, vi);
+	}
+}
