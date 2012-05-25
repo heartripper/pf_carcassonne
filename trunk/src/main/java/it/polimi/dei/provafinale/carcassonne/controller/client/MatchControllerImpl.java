8ebf882fc4ee75ec5e053f7c5975a823f9813696
ThinkyClaire <ThinkyClaire@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-25 12:34:59 +0000

    Fixed major problems in sonar.

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/controller/client/MatchControllerImpl.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/controller/client/MatchControllerImpl.java
@@ -50,7 +50,7 @@ public class MatchControllerImpl implements Runnable {
 			case TURN:
 				PlayerColor color = PlayerColor.valueOf(turnMsg.payload);
 				setCurrentPlayer(color);
-				if (color == clientPlayerColor) {
+				if (color.equals(clientPlayerColor)) {
 					manageClientTurn();
 				} else {
 					manageOtherPlayerTurn();
