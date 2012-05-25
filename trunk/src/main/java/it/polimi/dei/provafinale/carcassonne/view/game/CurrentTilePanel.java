4a3343bfb750db2fe1e688113dc0ab3fcb78e29d
ThinkyClaire <ThinkyClaire@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-25 20:24:34 +0000

    Added Javadoc.

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/view/game/CurrentTilePanel.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/view/game/CurrentTilePanel.java
@@ -9,40 +9,57 @@ import javax.swing.JPanel;
 
 import it.polimi.dei.provafinale.carcassonne.Constants;
 
-public class CurrentTilePanel extends JPanel{
-	
+public class CurrentTilePanel extends JPanel {
+
 	private static final long serialVersionUID = 1677387556314454292L;
-	
+
 	private String currentTileRep;
 	private JLabel tileLabel = null;
 	private TilePainter tilePainter = TilePainter.getInstance();
 	private Dimension dim;
-	
-	public CurrentTilePanel(){
+
+	/**
+	 * CurrentTilePanel constructor. Creates a new instance of class
+	 * CurrentTilePanel.
+	 */
+	public CurrentTilePanel() {
 		setLayout(new BorderLayout());
+
+		/* Setting dimension. */
 		int tileDim = Constants.TILE_PIXEL_DIMENSION;
 		dim = new Dimension(tileDim, tileDim);
-		
+
 		tileLabel = new JLabel();
 		tileLabel.setSize(dim);
 		tileLabel.setPreferredSize(dim);
 		tileLabel.setMaximumSize(dim);
 		tileLabel.setMinimumSize(dim);
-		
+
 		add(tileLabel);
 	}
-	
-	public void setCurrentTile(String rep){
+
+	/**
+	 * Draws the current tile graphic representation (empty tile or game tile,
+	 * depending on its textual identificator).
+	 * 
+	 * @param rep
+	 *            a String that represents the tile we want to represent.
+	 */
+	public void setCurrentTile(String rep) {
 		currentTileRep = rep;
 		Graphics g = tileLabel.getGraphics();
 		paintComponent(g);
 	}
-	
-	protected void paintComponent(Graphics g){
-		if(currentTileRep == null){
+
+	protected void paintComponent(Graphics g) {
+		/* There are no tiles corresponding to the given textual representation. */
+		if (currentTileRep == null) {
 			tilePainter.paintPlaceHolder(g, 0, 0);
-		}else{
+		}
+		/* There is one tile corresponding to the given textual representation. */
+		else {
 			tilePainter.paintTile(currentTileRep, g, 0, 0);
 		}
 	}
+
 }
