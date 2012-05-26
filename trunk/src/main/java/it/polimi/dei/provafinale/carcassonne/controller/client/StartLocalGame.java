827b5a181b8d1e08424d1dbb9fe7061158f2beb9
dariodotcom <dariodotcom@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-26 12:30:50 +0000

    Handles user selection of players number and view type in local game panel

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/controller/client/StartLocalGame.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/controller/client/StartLocalGame.java
@@ -1,18 +1,34 @@
 package it.polimi.dei.provafinale.carcassonne.controller.client;
 
+import it.polimi.dei.provafinale.carcassonne.Constants;
 import it.polimi.dei.provafinale.carcassonne.model.MatchHandler;
 import it.polimi.dei.provafinale.carcassonne.view.viewInterface.GUIViewInterface;
+import it.polimi.dei.provafinale.carcassonne.view.viewInterface.TextualViewInterface;
 import it.polimi.dei.provafinale.carcassonne.view.viewInterface.ViewInterface;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JComboBox;
+
 public class StartLocalGame implements ActionListener {
+
+	private final int[] NUM_PLAYER_VALUES = { 2, 3, 4, 5 };
+
+	private JComboBox numPlayerBox;
+	private JComboBox viewTypeBox;
+
+	public StartLocalGame(JComboBox numPlayerBox, JComboBox viewType) {
+		this.numPlayerBox = numPlayerBox;
+		this.viewTypeBox = viewType;
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
+		int numPlayers = NUM_PLAYER_VALUES[numPlayerBox.getSelectedIndex()];
+
 		// Create duplex interface for Client - Match controller communication
-		ClientLocalInterface cli = new ClientLocalInterface(2);
+		ClientLocalInterface cli = new ClientLocalInterface(numPlayers);
 
 		// Start match handler
 		MatchHandler mh = new MatchHandler(cli);
@@ -20,7 +36,16 @@ public class StartLocalGame implements ActionListener {
 		th.start();
 
 		// Create view interface
-		ViewInterface vi = new GUIViewInterface();
+		int viewType = viewTypeBox.getSelectedIndex();
+		ViewInterface vi;
+		if(viewType == Constants.VIEW_TYPE_GUI){
+			vi = new GUIViewInterface();
+		} else if(viewType == Constants.VIEW_TYPE_TEXTUAL){
+			vi = new TextualViewInterface();
+		} else {
+			System.out.println("Error in view type selection: value " + viewType);
+			return;
+		}
 
 		// Create client controller
 		ClientController.startNewMatchController(cli, vi);
