f818ccca8791fcb1e8b70a08a880e1fbb526350b
ThinkyClaire <ThinkyClaire@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-26 09:00:16 +0000

    Added Javadoc.

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/NotEnoughPlayersException.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/NotEnoughPlayersException.java
@@ -4,10 +4,17 @@ public class NotEnoughPlayersException extends Exception{
 
 	private static final long serialVersionUID = 5515480920138514643L;
 
+	/**
+	 * Manages the NotEnoughPlayersException.
+	 */
 	public NotEnoughPlayersException(){
 		super();
 	}
 	
+	/**
+	 * Manages the NotEnoughPlayersException.
+	 * @param s
+	 */
 	public NotEnoughPlayersException(String s){
 		super(s);
 	}
