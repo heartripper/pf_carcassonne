dbe9702f3adc9077181d98d6b935f5330c703c16
dariodotcom <dariodotcom@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-26 09:49:33 +0000

    Added javadoc

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/Constants.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/Constants.java
@@ -1,5 +1,8 @@
 package it.polimi.dei.provafinale.carcassonne;
 
+/**
+ * Class to hold constants used widely across the project.
+ * */
 public abstract class Constants {
 
 	public static final int SIDES_NUMBER = 4;
@@ -7,17 +10,17 @@ public abstract class Constants {
 	public static final int TILE_PIXEL_DIMENSION = 125;
 
 	public static final boolean ASK_ON_CLOSE = false;
-	
+
 	public static final String TILES_PATH = "src/main/resources/carcassonne.dat";
 	public static final String LESS_TILES_PATH = "src/main/resources/carcassonne_mini.dat";
-	
-	//Debug constants
+
+	// Debug constants
 	public static final boolean DEBUG_MODE = true;
 	public static final String DEBUG_ADDR = "localhost";
 	public static final int DEBUG_PORT = 12345;
 	public static final boolean USE_FEW_TILES = false;
-	
-	//Internet game panel.
+
+	// Internet game panel.
 	public static final String MATCH_IS_STARTING = "Connected to server. Waiting for match to begin.";
 	public static final String FIELDS_ERROR = "Please fill in all fields.";
 	public static final String PORT_ERROR = "Please insert a valid port value.";
