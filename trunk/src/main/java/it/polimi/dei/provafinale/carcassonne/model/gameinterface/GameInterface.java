efe96e35659dca034777391b996d37951adb9ae2
dariodotcom <dariodotcom@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-26 12:03:23 +0000

    Added javadoc

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/gameinterface/GameInterface.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/gameinterface/GameInterface.java
@@ -4,17 +4,50 @@ import it.polimi.dei.provafinale.carcassonne.model.gamelogic.player.PlayerColor;
 import it.polimi.dei.provafinale.carcassonne.model.server.PlayersDisconnectedException;
 
 /**
- * Interface between the game handler and game utilizers.
+ * Interface to let Game Controller communicate with Client Controller.
  */
 public interface GameInterface {
 
+	/**
+	 * Gives the number of players set by interface creator (server or user in
+	 * case of local game).
+	 * 
+	 * @return the number of player
+	 * */
 	public int askPlayerNumber();
 
-	public Message readFromPlayer(PlayerColor player)
+	/**
+	 * Reads a message from a player.
+	 * 
+	 * @param color
+	 *            - the PlayerColor of player to read from
+	 * @return the read Message
+	 * @throws PlayersDisconnectedException
+	 *             when given player disconnects
+	 * */
+	public Message readFromPlayer(PlayerColor color)
 			throws PlayersDisconnectedException;
 
+	/**
+	 * Sends a message to a player.
+	 * 
+	 * @param color
+	 *            - the color of the player to send the message to
+	 * @param msd
+	 *            - the Message to send
+	 * @throws PlayersDisconnectedException
+	 *             when given player disconnects
+	 * */
 	public void sendPlayer(PlayerColor color, Message msg)
 			throws PlayersDisconnectedException;
 
+	/**
+	 * Sends a message to all players.
+	 * 
+	 * @param msg
+	 *            - the Message to send
+	 * @throws PlayersDisconnectedException
+	 *             when one or more player discconnects;
+	 * */
 	public void sendAllPlayer(Message msg) throws PlayersDisconnectedException;
 }
