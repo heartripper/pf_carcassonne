6511878cc221db9612b2dd270f757f5b626abc17
dariodotcom <dariodotcom@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-26 11:53:09 +0000

    added javadoc

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/view/viewInterface/ViewInterface.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/view/viewInterface/ViewInterface.java
@@ -3,19 +3,65 @@ package it.polimi.dei.provafinale.carcassonne.view.viewInterface;
 import it.polimi.dei.provafinale.carcassonne.model.gamelogic.card.TileGrid;
 import it.polimi.dei.provafinale.carcassonne.model.gamelogic.player.PlayerColor;
 
+/**
+ * Interface to let Client Controller control an user interface.
+ * */
+
 public interface ViewInterface {
+	/**
+	 * Initializes the user interface.
+	 * 
+	 * @param grid
+	 *            - a tileGrid to be represented on the user interface
+	 * @param numPlayers
+	 *            - a int representing the number of players
+	 * @param clientColor
+	 *            - the color of player handled by controller, null if
+	 *            controller handles all players (local game)
+	 * */
 	public abstract void initialize(TileGrid grid, int numPlayers,
 			PlayerColor clientColor);
 
+	/**
+	 * Makes the user interface update the representation of the grid.
+	 * */
 	public abstract void updateGridRepresentation();
 
+	/**
+	 * Updates the current tile in the user interface.
+	 * 
+	 * @param - the string representation of a tile
+	 * */
 	public abstract void updateCurrentTile(String rep);
 
+	/**
+	 * Updates the score in the user interface
+	 * */
 	public abstract void updateScore(String msg);
 
-	public abstract void showNotify(String msg);
-	
+	/**
+	 * Enables or disables the user interface.
+	 * 
+	 * @param enabled
+	 *            - true if the user interface should be enabled, false
+	 *            otherwise
+	 * */
 	public abstract void setUIActive(boolean enabled);
-	
+
+	/**
+	 * Sets current player in the user interface.
+	 * 
+	 * @param color
+	 *            - the PlayerColor of current player
+	 * */
 	public abstract void setCurrentPlayer(PlayerColor color);
+
+	/**
+	 * Shows a notice on the user interface
+	 * 
+	 * @param msg
+	 *            - the notice to show
+	 * */
+	public abstract void showNotify(String msg);
+
 }
