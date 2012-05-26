6c3d30902e8c1d19b97ac349acc090aa3dd517e5
ThinkyClaire <ThinkyClaire@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-26 09:45:48 +0000

    Added Javadoc.

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/gameinterface/MessageBuffer.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/gameinterface/MessageBuffer.java
@@ -4,28 +4,44 @@ public class MessageBuffer {
 
 	private Message buffer = null;
 
+	/**
+	 * Provides a buffer to allow different threads to exchange messages.
+	 * 
+	 * @param msg
+	 *            a Message to write in the buffer.
+	 */
 	public synchronized void write(Message msg) {
+		/* The buffer hasn't been read yet. */
 		while (buffer != null) {
 			try {
 				wait();
 			} catch (InterruptedException e) {
 			}
 		}
-
+		/* Saves the written message in the buffer. */
 		buffer = msg;
+		/* Notifies all the waiting threads. */
 		notifyAll();
 	}
 
+	/**
+	 * Reads available message in the buffer.
+	 * 
+	 * @return the read Message.
+	 */
 	public synchronized Message read() {
+		/* If the buffer is empty, the current threads waits for a message. */
 		while (buffer == null) {
 			try {
 				wait();
 			} catch (InterruptedException e) {
 			}
 		}
-		
+		/* Saves the read message in msg. */
 		Message msg = buffer;
+		/* Empties the buffer. */
 		buffer = null;
+		/* Notifies all the waiting threads. */
 		notifyAll();
 		return msg;
 	}
