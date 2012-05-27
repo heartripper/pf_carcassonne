0fd81c5f69faabbc6bb3328aa674a5f670a0d822
ThinkyClaire <ThinkyClaire@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-27 13:40:11 +0000

    Added Javadoc.

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/gamelogic/tile/Tile.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/gamelogic/tile/Tile.java
@@ -6,7 +6,6 @@ import it.polimi.dei.provafinale.carcassonne.model.gamelogic.entity.EntityType;
 import it.polimi.dei.provafinale.carcassonne.model.gamelogic.player.PlayerColor;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,8 +17,7 @@ import java.util.Map;
 public final class Tile {
 	// Reference to container grid
 	private TileGrid grid;
-	private Coord cardCoord;
-
+	private Coord tileCoord;
 	private Side[] sides;
 	private List<SideConnection> connections;
 	private Map<String, String> linksCache;
@@ -29,40 +27,48 @@ public final class Tile {
 			SidePosition.S, SidePosition.W, SidePosition.E };
 
 	/**
-	 * Constructor: create a new Card entity.
+	 * Tile constructor. Creates a new entity of class Tile.
 	 * 
 	 * @param rep
-	 *            - a String representing the card according to the project
-	 *            specification
+	 *            - a String representing the Tile according to the project
+	 *            specification.
 	 */
 	public Tile(String rep) {
+		/* Find the Sides and the SideConnection of the given Tile. */
 		sides = new Side[Constants.SIDES_NUMBER];
 		connections = new ArrayList<SideConnection>();
-
+		/* rep analysis. */
 		String[] descriptors = rep.split(" ");
 		for (String desc : descriptors) {
 			String split[] = desc.split("=");
 			String name = split[0], value = split[1];
+			/* This part of rep represents one of the Side. */
 			if (name.length() == 1) {
 				setSide(name, value);
-			} else {
+			}
+			/* This part of rep represents one of the SideConnection */
+			else {
 				setConnection(name, value);
 			}
 		}
 	}
 
+	/* Setting Side options. */
 	private void setSide(String name, String value) {
 		int posIndex = SidePosition.valueOf(name).getIndex();
 		String sideType = null;
 		PlayerColor follower = null;
+		/* We are not analyzing the Side in presence of the follower. */
 		if (value.indexOf('-') == -1) {
 			sideType = value;
-		} else {
+		}
+		/* We are analyzing the Side in presence of the follower. */
+		else {
 			String[] split = value.split("-");
 			sideType = split[0];
 			follower = PlayerColor.valueOf(split[1]);
 		}
-
+		/* Setting Side parameters. */
 		EntityType type = EntityType.valueOf(sideType);
 		Side side = new Side(this, type);
 		sides[posIndex] = side;
@@ -71,48 +77,56 @@ public final class Tile {
 		}
 	}
 
+	/* Setting connection options. */
 	private void setConnection(String name, String value) {
+		/*
+		 * Conversion of value in number: there is no connection between two
+		 * sides.
+		 */
 		if (Integer.parseInt(value) == 0) {
 			return;
 		}
+		/*
+		 * There is a connection between two sides: setting start and end of
+		 * connection.
+		 */
 		String start = String.valueOf(name.charAt(0));
 		String end = String.valueOf(name.charAt(1));
-
 		Side side1 = getSide(SidePosition.valueOf(start));
 		Side side2 = getSide(SidePosition.valueOf(end));
 		connections.add(new SideConnection(side1, side2));
 	}
 
 	/**
-	 * Set the card on a grid in a specific position.
+	 * Sets the tile on a grid in a specific position.
 	 * 
 	 * @param grid
-	 *            - a Grid where the card is put on.
+	 *            - a Grid where we want to put the tile on.
 	 * @param coord
-	 *            - a Coord where the card is collocate.
+	 *            - a Coord where we want to put the tile.
 	 */
 	public void setPositionInfo(TileGrid grid, Coord coord) {
 		this.grid = grid;
-		this.cardCoord = coord;
+		this.tileCoord = coord;
 	}
 
 	/**
-	 * Find a side (with its entity type, entity, owner and follower) of a card.
+	 * Find a side (with its entity type, entity, owner and follower) of a tile.
 	 * 
 	 * @param side
-	 *            - a SidePosition of a card.
-	 * @return a Side of the card.
+	 *            - a SidePosition of the Tile.
+	 * @return a Side of the tile.
 	 */
 	public Side getSide(SidePosition position) {
 		return sides[position.getIndex()];
 	}
 
 	/**
-	 * Give the side position of a given side of a card.
+	 * Give the SidePosition corresponding to a given Side of a tile.
 	 * 
 	 * @param side
-	 *            - a Side of a Card.
-	 * @return the SidePosition of the given Side of the card.
+	 *            - a Side of the Tile.
+	 * @return the SidePosition of the given Side of the Tile.
 	 */
 	public SidePosition getSidePosition(Side side) {
 		for (SidePosition position : SidePosition.values()) {
@@ -124,10 +138,9 @@ public final class Tile {
 	}
 
 	/**
-	 * Tries to add a follower to the side corresponding to the given position in
-	 * the current card.
+	 * Adds a follower of the given PlayerColor to the given SidePosition in
+	 * Tile.
 	 * 
-	 * @return true if the follower was added, false if it couldn't be added.
 	 * */
 	public void addFollower(SidePosition position, PlayerColor color) {
 		Side side = getSide(position);
@@ -135,62 +148,65 @@ public final class Tile {
 	}
 
 	/**
-	 * Gives this card's coordinates.
 	 * 
-	 * @return this card coordinates.
+	 * @return the Tile coordinates.
 	 */
 	public Coord getCoordinates() {
-		return cardCoord;
+		return tileCoord;
 	}
 
 	/**
-	 * Give the card opposite to a given side of a the card.
+	 * Gives the Tile opposite to a given SidePosition of the current Tile.
 	 * 
 	 * @param position
-	 *            - a SidePosition of this card
-	 * @return the neighbor Card.
+	 *            - a SidePosition of the current Tile.
+	 * @return the neighbor Tile.
 	 */
 	public Tile getNeighbor(SidePosition position) {
 		Coord offset;
-		if (grid == null || cardCoord == null) {
+		/* There is no neighbor. */
+		if (grid == null || tileCoord == null) {
 			return null;
 		}
+		/* Finds the neighbor. */
 		offset = SidePosition.getOffsetForPosition(position);
-
-		Coord neighborCoord = cardCoord.add(offset);
+		Coord neighborCoord = tileCoord.add(offset);
 		return grid.getTile(neighborCoord);
 	}
 
 	/**
-	 * Give a list of the sides linked to a given one in a card because of the
-	 * presence of an entity.
+	 * Provides a list of the Side linked to a given one (side1) in a tile
+	 * because of the presence of an entity.
 	 * 
-	 * @param side
-	 *            - a Side.
-	 * @return an ArrayList of Side linked to a given one in the card.
+	 * @param side1
+	 *            - a Side we want to know the related ones.
+	 * @return an ArrayList of Side linked to the given one in a Tile.
 	 */
 	public List<Side> sidesLinkedTo(Side side1) {
-		List<Side> sides = new ArrayList<Side>();
+		List<Side> linkedSides = new ArrayList<Side>();
 		for (Side side2 : this.sides) {
+			/* Examination of a couple of sides of the same Tile. */
 			if (side1 != side2) {
+				/* Create the connection between side1 and side2. */
 				SideConnection connection = new SideConnection(side1, side2);
+				/* Check if the connection really exists. */
 				if (connections.contains(connection)) {
-					sides.add(side2);
+					linkedSides.add(side2);
 				}
 			}
 		}
-		return sides;
+		return linkedSides;
 	}
 
 	/**
-	 * Check the presence of a link between two sides of a given card.
+	 * Checks the presence of a link between two sides of a given card.
 	 * 
-	 * @param side1
-	 *            - a Side.
-	 * @param side2
-	 *            - a Side.
-	 * @return true if there is an entity that links side1 and side2 of the
-	 *         card, false instead.
+	 * @param start
+	 *            a SidePosition.
+	 * @param end
+	 *            a SidePosition.
+	 * @return true if there is an entity thar links start and end in the given
+	 *         Tile.
 	 */
 	public boolean sideLinked(SidePosition start, SidePosition end) {
 		Side side1 = getSide(start);
@@ -199,15 +215,14 @@ public final class Tile {
 	}
 
 	/**
-	 * Rotate the card of 90ï¿½ clock wise.
+	 * Rotate the Tile of 90 degrees clock wise.
 	 */
 	public void rotate() {
-		// If tile has already been placed, it can't be rotated
-		if (cardCoord != null) {
+		/* If tile has already been placed, it can't be rotated. */
+		if (tileCoord != null) {
 			return;
 		}
-
-		
+		/* Rotation implementation. */
 		int i = sides.length - 1;
 		Side backup = sides[i];
 		for (; i > 0; i--) {
@@ -216,32 +231,30 @@ public final class Tile {
 		sides[0] = backup;
 	}
 
-	/**
-	 * Gives the string representing this card, according to project
-	 * specification.
-	 * 
-	 * @return the String representing this card.
-	 * */
 	@Override
 	public String toString() {
 		StringBuilder representation = new StringBuilder();
-		// Sides representation
+		/* Sides representation. */
 		for (SidePosition pos : representationOrder) {
 			String rep = String.format("%s=%s ", pos, getSideRep(pos));
 			representation.append(rep);
 		}
-		// Connections representation
-		if (!linksCacheValid) {
+		/* Connections representation. */
+		/* Case the tile has been rotated. */
+		if (linksCacheValid == false) {
 			linksCache = new HashMap<String, String>();
 			for (int i = 0; i < representationOrder.length; i++) {
 				for (int j = 0; j < representationOrder.length; j++) {
+					/* Not necessary to confront two times. */
 					if (i >= j) {
 						continue;
 					}
+					/* New SidePosition assignment. */
 					SidePosition pos1 = representationOrder[i];
 					SidePosition pos2 = representationOrder[j];
 					SideConnection con = new SideConnection(getSide(pos1),
 							getSide(pos2));
+					/* Setting connections. */
 					boolean connected = connections.contains(con);
 					int val = (connected ? 1 : 0);
 					String conn = String.format("%s%s", pos1, pos2);
@@ -251,7 +264,7 @@ public final class Tile {
 			}
 
 		}
-
+		/* Adds the current String to the string builder to create the complete representation. */
 		representation.append(linksCache.get("NS"));
 		representation.append(linksCache.get("NE"));
 		representation.append(linksCache.get("NW"));
@@ -261,9 +274,11 @@ public final class Tile {
 		return representation.toString().trim();
 	}
 
+	/* Return the String representation of a given SidePosition. */
 	private String getSideRep(SidePosition pos) {
 		String rep = getSide(pos).getType().toString();
 		PlayerColor follower = getSide(pos).getFollower();
+		/* Analyzing follower option. */
 		String col = (follower == null ? null : follower.toString());
 		if (col != null) {
 			rep += ("-" + col);
