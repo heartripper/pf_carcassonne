109db51d059ba9a5b74a523d0acd6ecd7d186110
dariodotcom <dariodotcom@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-27 13:16:00 +0000

    Add main class to start GUI

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/MainClass.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/MainClass.java
@@ -1,12 +1,9 @@
 package it.polimi.dei.provafinale.carcassonne;
 
-import it.polimi.dei.provafinale.carcassonne.model.MatchHandler;
-import it.polimi.dei.provafinale.carcassonne.model.textinterface.TextualInterface;
+import it.polimi.dei.provafinale.carcassonne.view.ViewManager;
 
 public class MainClass {
 	public static void main(String[] args) {
-		TextualInterface textIf = new TextualInterface(System.out, System.in);
-		MatchHandler mh = new MatchHandler(textIf);
-		mh.startGame();
+		ViewManager.getInstance();
 	}
 }
--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/view/MainClass.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.dei.provafinale.carcassonne.view;
-
-public class MainClass {
-	public static void main(String[] args) {
-		ViewManager.getInstance();
-	}
-}
