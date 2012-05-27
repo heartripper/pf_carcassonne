3232c13744f71f00ca86c3162d7f59457667194d
ThinkyClaire <ThinkyClaire@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-27 13:38:07 +0000

    Added Javadoc.

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/gamelogic/tile/SideConnection.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/gamelogic/tile/SideConnection.java
@@ -10,10 +10,14 @@ public class SideConnection {
 	private Side side2;
 
 	/**
-	 * SideConnection constructor. Creates a new instance of class SideConnection (constructs a SideConnection instance given the two sides that are
-	 * connected).
-	 * @param side1 - a Side.
-	 * @param side2 - a Side.
+	 * SideConnection constructor. Creates a new instance of class
+	 * SideConnection (constructs a SideConnection instance given the two sides
+	 * that are connected).
+	 * 
+	 * @param side1
+	 *            - a Side.
+	 * @param side2
+	 *            - a Side.
 	 * */
 	public SideConnection(Side side1, Side side2) {
 		this.side1 = side1;
@@ -27,12 +31,14 @@ public class SideConnection {
 
 	@Override
 	public boolean equals(Object obj) {
+		/* The passed object is not an instance of SideConnection. */
 		if (!(obj instanceof SideConnection)) {
 			return false;
 		}
+		/* Checks if the passed SideConnection is equal to the current one. */
 		SideConnection otherConn = (SideConnection) obj;
 		return (side1 == otherConn.side1 && side2 == otherConn.side2)
 				|| (side1 == otherConn.side2 && side2 == otherConn.side1);
 	}
-	
+
 }
