7828c47d710dba6b06f2f6f895b92a9985300019
Chiara <kia90yaya@hotmail.it>
2012-05-27 09:08:09 +0000

    Rinominato package.

--- src/main/java/it/polimi/dei/provafinale/carcassonne/model/MatchHandler.java
+++ src/main/java/it/polimi/dei/provafinale/carcassonne/model/MatchHandler.java
@@ -7,9 +7,9 @@ import it.polimi.dei.provafinale.carcassonne.model.gameinterface.Message;
 import it.polimi.dei.provafinale.carcassonne.model.gameinterface.MessageType;
 import it.polimi.dei.provafinale.carcassonne.model.gamelogic.Coord;
 import it.polimi.dei.provafinale.carcassonne.model.gamelogic.Match;
-import it.polimi.dei.provafinale.carcassonne.model.gamelogic.card.Tile;
-import it.polimi.dei.provafinale.carcassonne.model.gamelogic.card.SidePosition;
 import it.polimi.dei.provafinale.carcassonne.model.gamelogic.player.PlayerColor;
+import it.polimi.dei.provafinale.carcassonne.model.gamelogic.tile.SidePosition;
+import it.polimi.dei.provafinale.carcassonne.model.gamelogic.tile.Tile;
 import it.polimi.dei.provafinale.carcassonne.model.server.PlayersDisconnectedException;
 
 public class MatchHandler implements Runnable {
