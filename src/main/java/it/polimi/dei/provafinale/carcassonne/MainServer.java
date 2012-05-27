e26b1e696233608d974190bc2b9d8a8f319c19c8
dariodotcom <heartripper@users.noreply.github.com>
2012-05-27 13:43:17 +0000

    Start server accepting both RMI and socket connections

--- src/main/java/it/polimi/dei/provafinale/carcassonne/MainServer.java
+++ src/main/java/it/polimi/dei/provafinale/carcassonne/MainServer.java
@@ -4,11 +4,7 @@ import it.polimi.dei.provafinale.carcassonne.controller.server.CarcassonneServer
 
 public class MainServer {
 	public static void main(String[] args) {
-		/*V1*/
-		//(new CarcassonneServer(12345)).startRequestMonitor();
-		
-		/*V2*/
-		CarcassonneServer server = new CarcassonneServer(CarcassonneServer.RMI, 12345);
+		CarcassonneServer server = new CarcassonneServer(CarcassonneServer.BOTH, 12345);
 		server.start();
 	}
 }
