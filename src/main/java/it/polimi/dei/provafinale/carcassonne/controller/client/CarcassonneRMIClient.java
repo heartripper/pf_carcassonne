b8856f4218c0f9acfd86261dbc3d5f18cfeb90b3
dariodotcom <heartripper@users.noreply.github.com>
2012-05-28 12:01:17 +0000

    Added javadoc

--- src/main/java/it/polimi/dei/provafinale/carcassonne/controller/client/CarcassonneRMIClient.java
+++ src/main/java/it/polimi/dei/provafinale/carcassonne/controller/client/CarcassonneRMIClient.java
@@ -5,7 +5,22 @@ import it.polimi.dei.provafinale.carcassonne.model.gameinterface.Message;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * Remote interface of RMI Client
+ * */
 public interface CarcassonneRMIClient extends Remote {
+
+	/**
+	 * Sends a message to client.
+	 * 
+	 * @param msg
+	 *            - the Message to send to client
+	 * */
 	void sendMessageToPlayer(Message msg) throws RemoteException;
+
+	/**
+	 * Reads a message from the client.
+	 * @return the Message read from client
+	 * */
 	Message readMessageFromPlayer() throws RemoteException;
 }
