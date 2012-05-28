1c63512e770914ae16e75418bf4b5a3568131844
dariodotcom <heartripper@users.noreply.github.com>
2012-05-28 08:10:27 +0000

    Implement Logger service

---
+++ src/main/java/it/polimi/dei/provafinale/carcassonne/logger/LoggerService.java
@@ -0,0 +1,41 @@
+package it.polimi.dei.provafinale.carcassonne.logger;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+
+public class LoggerService {
+
+	private final int width = 800;
+	private final int height = 600;
+	
+	private JTabbedPane tabs;
+	private static LoggerService instance = null;
+	
+	
+	public static LoggerService getService(){
+		if(instance == null){
+			instance = new LoggerService();
+		}
+		
+		return instance;
+	}
+	
+	private LoggerService() {
+		JFrame frame = new JFrame("Logger");
+		frame.setSize(width, height);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		tabs = new JTabbedPane();
+		frame.add(tabs, BorderLayout.CENTER);
+		frame.setVisible(true);
+	}
+	
+	public Logger register(String panelName){
+		Logger logger = new Logger();
+		tabs.addTab(panelName, logger);
+		return logger;
+	}
+
+}
