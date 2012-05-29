a8ce2798d3606ea051e47587189a9aa93f4918f3
ThinkyClaire <ThinkyClaire@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-29 14:06:31 +0000

    Finito il Javadoc

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/view/menu/HomePanel.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/view/menu/HomePanel.java
@@ -18,39 +18,53 @@ public class HomePanel extends JPanel {
 	private static final long serialVersionUID = 8988417171482193429L;
 	private Image background;
 
+	/**
+	 * HomePanel constructor. Creates a new instance of class HomePanel.
+	 * 
+	 * @param background
+	 *            an Image we want to set as background of the panel.
+	 */
 	public HomePanel(Image background) {
+		/* Setting background attribute. */
 		this.background = background;
 
+		/* Setting layout. */
 		BoxLayout layout = new BoxLayout(this, BoxLayout.Y_AXIS);
 		setLayout(layout);
 
+		/* Layout option. */
 		Component verticalGlue01 = Box.createVerticalGlue();
 
+		/* Adding new local game panel. */
 		JPanel newLocalGamePanel = new JPanel();
 		newLocalGamePanel.setBackground(new Color(0, 0, 0, 0));
 		newLocalGamePanel.setLayout(new BoxLayout(newLocalGamePanel,
 				BoxLayout.X_AXIS));
+		/* Adding new local game button to new local game panel. */
 		JButton btnNewLocalGame = new JButton("New Local Game");
 		newLocalGamePanel.add(btnNewLocalGame);
 		ActionListener local = new MenuPanelSwitcher(MenuPanel.LOCALGAMEPANEL);
 		btnNewLocalGame.addActionListener(local);
 
+		/* Layout option. */
 		Component verticalStrut = Box.createVerticalStrut(20);
 
+		/* Adding new internet game panel. */
 		JPanel newInternetGamePanel = new JPanel();
 		newInternetGamePanel.setBackground(new Color(0, 0, 0, 0));
 		newInternetGamePanel.setLayout(new BoxLayout(newInternetGamePanel,
 				BoxLayout.X_AXIS));
-
+		/* Adding new internet game button to new internet game panel. */
 		JButton btnNewInternetGame = new JButton("New Internet Game");
 		newInternetGamePanel.add(btnNewInternetGame);
 		ActionListener internet = new MenuPanelSwitcher(
 				MenuPanel.INTERNETGAMEPANEL);
 		btnNewInternetGame.addActionListener(internet);
 
+		/* Layout option. */
 		Component verticalGlue02 = Box.createVerticalGlue();
 
-		// Add components to panel
+		/* Add components to panel. */
 		add(verticalGlue01);
 		add(newLocalGamePanel);
 		add(verticalStrut);
@@ -60,6 +74,9 @@ public class HomePanel extends JPanel {
 		repaint();
 	}
 
+	/**
+	 * Paints the background image on the panel.
+	 */
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		int width = getWidth();
