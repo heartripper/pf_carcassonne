2c3b1b1989372a2863285410fba2a508f079a0cb
Chiara <kia90yaya@hotmail.it>
2012-05-29 11:29:50 +0000

    Finito il Javadoc

--- src/main/java/it/polimi/dei/provafinale/carcassonne/view/game/PlayerPanel.java
+++ src/main/java/it/polimi/dei/provafinale/carcassonne/view/game/PlayerPanel.java
@@ -24,85 +24,138 @@ public class PlayerPanel extends JPanel {
 	private JLabel lblScore;
 	private JLabel lblFollower;
 
-	// Background colors
+	/* Background colors. */
 	private final Color selectedBGColor = new Color(255, 255, 255);
 	private final Color unselectedBGColor = new Color(225, 225, 225);
 	private final Color transparentBGColor = new Color(0, 0, 0, 0);
 
-	// Fonts
+	/* Fonts. */
 	private final Font selNameFont = new Font(Font.SANS_SERIF, Font.BOLD, 15);
 	private final Font unselNameFont = new Font(Font.SANS_SERIF, Font.PLAIN, 15);
 	private final Font commonFont = new Font(Font.SANS_SERIF, Font.PLAIN, 13);
 
-	// Dimension
+	/* Dimension. */
 	private final Dimension panelDimension = new Dimension(135, 70);
-	private final Dimension labelDimension = new Dimension(10,20);
+	private final Dimension labelDimension = new Dimension(10, 20);
 	private final Dimension colorDimension = new Dimension(20, 15);
 
+	/**
+	 * PlayerPanel constructor. Creates a new instance of class PlayerPanel.
+	 * 
+	 * @param playerColor
+	 *            the color of the player, which the panel refers to.
+	 */
 	public PlayerPanel(PlayerColor playerColor) {
-		setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		this.playerColor = playerColor;
 
+		setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		setPreferredSize(panelDimension);
 		setLayout(new BorderLayout());
 
+		this.playerColor = playerColor;
+
+		/*
+		 * Setting the color panel (to put on the left side of player
+		 * informations).
+		 */
 		colorPanel = new JPanel();
 		colorPanel.setBackground(playerColor.getColor());
 		colorPanel.setPreferredSize(colorDimension);
 		add(colorPanel, BorderLayout.WEST);
 
+		/*
+		 * Setting content panel, which contains all the informations about a
+		 * player (name, score, follower).
+		 */
 		JPanel content = new JPanel();
 		content.setBackground(transparentBGColor);
-		add(content, BorderLayout.CENTER);
 		content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
-
-		String name = playerColor.getFullName();
+		add(content, BorderLayout.CENTER);
+		/*
+		 * Setting name panel (contains the name that identifies a player) to
+		 * put into content panel.
+		 */
 		JPanel namePanel = new JPanel();
+		String name = playerColor.getFullName();
+		lblName = new JLabel(name);
 		namePanel.setBackground(transparentBGColor);
 		namePanel.setPreferredSize(labelDimension);
-		lblName = new JLabel(name);
 		namePanel.add(lblName);
-
+		/*
+		 * Setting score panel (contains the actual score of a player) to put
+		 * into content panel.
+		 */
 		JPanel scorePanel = new JPanel();
-		scorePanel.setPreferredSize(labelDimension);
-		scorePanel.setBackground(transparentBGColor);
 		lblScore = new JLabel("Score: 0");
 		lblScore.setFont(commonFont);
+		scorePanel.setPreferredSize(labelDimension);
+		scorePanel.setBackground(transparentBGColor);
 		scorePanel.add(lblScore);
-
+		/*
+		 * Setting followers panel (contains the actual number of players that
+		 * belongs to a player) to put into content panel.
+		 */
 		JPanel followerPanel = new JPanel();
-		followerPanel.setPreferredSize(labelDimension);
-		followerPanel.setBackground(transparentBGColor);
 		lblFollower = new JLabel("Follower: 7");
 		lblFollower.setFont(commonFont);
+		followerPanel.setPreferredSize(labelDimension);
+		followerPanel.setBackground(transparentBGColor);
 		followerPanel.add(lblFollower);
-
+		/* Adding panels to content panel. */
 		content.add(namePanel);
 		content.add(scorePanel);
 		content.add(followerPanel);
 	}
 
+	/**
+	 * Sets the actual score of a player into the JLabel lblScore (attribute of
+	 * the class PlayerPanel).
+	 * 
+	 * @param score
+	 *            the score we want to add to the current one.
+	 */
 	public void setScore(int score) {
 		lblScore.setText("Score: " + score);
 	}
 
+	/**
+	 * Sets the actual number of followers that belongs to a player into the
+	 * JLabel lblFollowers (attribute of the class PlayerPanel).
+	 * 
+	 * @param numFollowers
+	 *            a number that indicates the current number of followers of a
+	 *            player.
+	 */
 	public void setFollowers(int numFollowers) {
 		lblFollower.setText("Follower: " + numFollowers);
 	}
 
+	/**
+	 * Sets the status of a player: if a player is active the background color
+	 * of his panel will be white and the font-weight bold, otherwise of the
+	 * default color and the default font-size.
+	 * 
+	 * @param active
+	 *            if true indicates that is the player turn, if false indicates
+	 *            that is the turn of another player.
+	 */
 	public void setActive(boolean active) {
 		Font f;
+		/* It's current player turn. */
 		if (active) {
 			f = selNameFont;
 			setBackground(selectedBGColor);
-		} else {
+		}
+		/* It's another player turn. */
+		else {
 			f = unselNameFont;
 			setBackground(unselectedBGColor);
 		}
-
 		lblName.setFont(f);
 	}
 
+	/**
+	 * Sets this player as the one playing on the client.
+	 */
 	public void setClientPlayer() {
 		lblName.setText(playerColor.getFullName() + " (You)");
 	}
