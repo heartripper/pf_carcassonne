58766075e266482ea21cb71889f061656479058e
Chiara <kia90yaya@hotmail.it>
2012-05-29 07:39:49 +0000

    Aggiunta buona parte di javadoc.

--- src/main/java/it/polimi/dei/provafinale/carcassonne/view/game/SwingGamePanel.java
+++ src/main/java/it/polimi/dei/provafinale/carcassonne/view/game/SwingGamePanel.java
@@ -24,10 +24,13 @@ public class SwingGamePanel extends GamePanel {
 
 	private static final long serialVersionUID = -5552501660516939765L;
 
+	/* Tiles area. */
 	private JPanel tilesArea;
 	private TilesPanel tilesPanel;
+	/* Overview of players area. */
 	private JPanel players;
 	private PlayerPanel[] playerPanels;
+	/* Notifications area. */
 	private CurrentTilePanel currentTilePanel;
 	private JLabel messageLabel;
 	private JButton rotateButton;
@@ -38,99 +41,129 @@ public class SwingGamePanel extends GamePanel {
 	private JButton passButton;
 
 	/**
-	 * SwingGamePanel constructor. Creates a new instance of class SwingGamePanel.
+	 * SwingGamePanel constructor. Creates a new instance of class
+	 * SwingGamePanel.
 	 */
 	public SwingGamePanel() {
-		/*Setting the class layout.*/
+
+		/* Setting the class layout. */
 		setLayout(new BorderLayout(0, 0));
-		/*Initializing the area where to put tiles.*/
+
+		/* Initializing the area where to put tiles. */
 		tilesArea = new JPanel();
 		tilesArea.setLayout(new BorderLayout());
 		add(tilesArea, BorderLayout.CENTER);
 
-		// Notification panel
+		/*
+		 * Initializing the panel where to put notifications (e.g. current card,
+		 * current player turn, rotation option, field to put coordinates with
+		 * the put button, the option to add a follower with the put option and
+		 * the option to conclude the turn).
+		 */
 		JPanel actionsPanel = new JPanel();
 		actionsPanel.setPreferredSize(new Dimension(195, 10));
 		actionsPanel.setBorder(new MatteBorder(0, 1, 0, 0, (Color) Color.GRAY));
-		add(actionsPanel, BorderLayout.EAST);
 		actionsPanel.setLayout(new BoxLayout(actionsPanel, BoxLayout.Y_AXIS));
+		add(actionsPanel, BorderLayout.EAST);
 
-		// Current Tile panel
+		/* Notifications panel. */
+
 		JPanel noticationsPanel = new JPanel();
 		noticationsPanel.setLayout(new FlowLayout());
+		/* Tile panel. */
 		currentTilePanel = new CurrentTilePanel();
 		noticationsPanel.add(currentTilePanel, BorderLayout.CENTER);
+		/* Message label. */
 		messageLabel = new JLabel();
 		noticationsPanel.add(messageLabel, BorderLayout.SOUTH);
 
-		// Rotate panel
+		/* Rotate panel. */
+
 		JPanel rotatePanel = new JPanel();
 		rotatePanel.setLayout(new BoxLayout(rotatePanel, BoxLayout.X_AXIS));
+		/* Rotate button. */
 		rotateButton = new JButton("Rotate");
 		ActionListener rotate = new MessageSender(MessageType.ROTATE, null);
 		rotateButton.addActionListener(rotate);
 		rotatePanel.add(rotateButton);
 
-		// Coords panel
+		/* Coordinate panel. */
+
 		JPanel coordsPanel = new JPanel();
 		coordsPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		/* Insert coordinate label. */
 		JLabel lblInsertCoordinatesxy = new JLabel("Insert coordinates (x,y):");
 		coordsPanel.add(lblInsertCoordinatesxy);
+		/* Coordinates text field. */
 		coordsField = new JTextField();
 		coordsField.setColumns(10);
 		coordsPanel.add(coordsField);
+		/* Put button. */
 		putTileButton = new JButton("Put tile");
 		ActionListener place = new MessageSender(MessageType.PLACE, coordsField);
 		putTileButton.addActionListener(place);
 		coordsPanel.add(putTileButton);
 
-		// Follower panel
+		/* Follower panel. */
+
 		JPanel followerPanel = new JPanel();
+		/* Follower label. */
 		JLabel lblFollower = new JLabel("Follower:");
 		followerPanel.add(lblFollower);
+		/* Follower options combo box. */
 		String[] followerOptions = { "north", "east", "south", "west" };
 		followerComboBox = new JComboBox(followerOptions);
 		followerPanel.add(followerComboBox);
+		/* Put follower button. */
 		putFollowerButton = new JButton("Put follower");
 		ActionListener follower = new MessageSender(MessageType.FOLLOWER,
 				followerComboBox);
 		putFollowerButton.addActionListener(follower);
 		followerPanel.add(putFollowerButton);
 
-		// Pass button
+		/* Pass panel. */
 		JPanel passPanel = new JPanel();
+		/* Pass button. */
 		passButton = new JButton("Pass");
 		ActionListener pass = new MessageSender(MessageType.PASS, null);
 		passButton.addActionListener(pass);
 		passPanel.add(passButton);
 
+		/* Add the panels to the main panel. */
 		actionsPanel.add(noticationsPanel);
 		actionsPanel.add(rotatePanel);
 		actionsPanel.add(coordsPanel);
 		actionsPanel.add(followerPanel);
 		actionsPanel.add(passPanel);
 
+		/* Player (bottom) panel. */
+
 		JPanel bottomPanel = new JPanel();
 		bottomPanel.setBorder(new MatteBorder(1, 0, 0, 0, (Color) Color.GRAY));
-		add(bottomPanel, BorderLayout.SOUTH);
 		bottomPanel.setLayout(new BorderLayout(0, 0));
-
-		// Options panel
+		add(bottomPanel, BorderLayout.SOUTH);
+		/* Options Panel */
 		JPanel optionsPanel = new JPanel();
 		optionsPanel.setLayout(new BoxLayout(optionsPanel, BoxLayout.Y_AXIS));
 		bottomPanel.add(optionsPanel, BorderLayout.EAST);
+		/* Back to home panel (to put into options panel). */
 		JPanel pnlBackToHome = new JPanel();
+		optionsPanel.add(pnlBackToHome);
+		/* Back to home button. */
 		JButton btnBackToHome = new JButton("Back to Home");
 		pnlBackToHome.add(btnBackToHome);
+		/* Screenshot panel (to put into options panel). */
 		JPanel pnlScreenshot = new JPanel();
+		optionsPanel.add(pnlScreenshot);
+		/* Take a screenshot button. */
 		JButton btnTakeAScreenshot = new JButton("Take a ScreenShot");
 		pnlScreenshot.add(btnTakeAScreenshot);
-		optionsPanel.add(pnlBackToHome);
-		optionsPanel.add(pnlScreenshot);
-
+		/* Players panel (to put into options panel). */
 		players = new JPanel();
 		bottomPanel.add(players, BorderLayout.WEST);
 	}
+
+	/* Implementation of ViewInterface methods. */
 
 	@Override
 	public void initialize(TileGrid grid, int numPlayers,
@@ -138,7 +171,6 @@ public class SwingGamePanel extends GamePanel {
 		/* Create tilesPanel */
 		tilesPanel = new TilesPanel(grid);
 		tilesArea.add(tilesPanel, BorderLayout.CENTER);
-
 		/* Setup players */
 		playerPanels = new PlayerPanel[numPlayers];
 		for (int i = 0; i < numPlayers; i++) {
@@ -146,20 +178,18 @@ public class SwingGamePanel extends GamePanel {
 			players.add(p);
 			playerPanels[i] = p;
 		}
-
 		/* Setup client player color */
 		if (clientColor != null) {
 			int index = PlayerColor.indexOf(clientColor);
 			playerPanels[index].setClientPlayer();
 		}
-
 		/* Disable UI */
 		setUIActive(false);
-
 		/* Update representation of tiles */
 		updateGridRepresentation();
 	}
 
+	/* Prints the current grid on the special area. */
 	@Override
 	public void updateGridRepresentation() {
 		tilesPanel.updateRepresentation();
@@ -167,11 +197,13 @@ public class SwingGamePanel extends GamePanel {
 		tilesPanel.paint(g);
 	}
 
+	/* Prints the representation of the current tile on the special area. */
 	@Override
 	public void updateCurrentTile(String rep) {
 		currentTilePanel.setCurrentTile(rep);
 	}
 
+	/* Gives the current scores of the players that are playing the match. */
 	@Override
 	public void updateScore(String msg) {
 		String[] scores = msg.split(",");
@@ -184,6 +216,7 @@ public class SwingGamePanel extends GamePanel {
 		}
 	}
 
+	/* Gives informations about who is the turn of. */
 	@Override
 	public void setCurrentPlayer(PlayerColor color) {
 		int selectedIndex = PlayerColor.indexOf(color);
@@ -192,6 +225,7 @@ public class SwingGamePanel extends GamePanel {
 		}
 	}
 
+	/* Prints notifications on the textArea. */
 	@Override
 	public void showNotify(String msg) {
 		messageLabel.setText(msg);
@@ -206,4 +240,5 @@ public class SwingGamePanel extends GamePanel {
 		putFollowerButton.setEnabled(enabled);
 		passButton.setEnabled(enabled);
 	}
+
 }
