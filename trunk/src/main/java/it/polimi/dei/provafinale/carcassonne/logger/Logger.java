da04e22ad24f47ebfa37adef032af9e128865d97
ThinkyClaire <ThinkyClaire@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-29 20:13:53 +0000

    Finito il javadoc.

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/logger/Logger.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/logger/Logger.java
@@ -7,23 +7,43 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-public class Logger extends JPanel{
+/**
+ * The Logger class extends a JPanel and serves to show any message on the
+ * simulated console.
+ * 
+ */
+public class Logger extends JPanel {
 
 	private static final long serialVersionUID = -8795155705201105543L;
+
 	private StringBuilder log;
 	private JTextArea textArea;
 
+	/**
+	 * Class Logger constructor. Creates a new instance of class Logger.
+	 */
 	public Logger() {
+		/* Setting layout. */
 		setLayout(new BorderLayout());
+		/* Setting textArea. */
 		textArea = new JTextArea();
 		textArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 14));
 		JScrollPane scroll = new JScrollPane(textArea);
 		add(scroll, BorderLayout.CENTER);
+		/* Creating a StringBuilder. */
 		log = new StringBuilder();
 	}
-	
-	public void log(String msg){
+
+	/**
+	 * Displays a message in textArea.
+	 * 
+	 * @param msg
+	 *            the String message we want to display on the textArea.
+	 */
+	public void log(String msg) {
+		/* Formatting msg. */
 		log.append(msg + "\n");
+		/* Setting msg content on the textArea. */
 		textArea.setText(log.toString());
 	}
 }
