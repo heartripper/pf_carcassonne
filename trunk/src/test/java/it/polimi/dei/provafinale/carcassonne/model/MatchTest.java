8bbdd9b024c2883e01a7e101b9175e740688d269
dariodotcom <dariodotcom@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-30 11:52:19 +0000

    Added some tests to MatchTest

--- trunk/src/test/java/it/polimi/dei/provafinale/carcassonne/model/MatchTest.java
+++ trunk/src/test/java/it/polimi/dei/provafinale/carcassonne/model/MatchTest.java
@@ -34,7 +34,15 @@ public class MatchTest {
 		assertFalse(match.putTile(tiles[5], new Coord(0, 1)));
 		/* Checks that we can add a tile in a compatible position. */
 		assertTrue(match.putTile(tiles[2], new Coord(0, -1)));
-		
+		assertTrue(match.putTile(tiles[1], new Coord(-1, -1)));
+		/* Check opposite sides */
+		Side s1 = tiles[1].getSide(SidePosition.E);
+		Side s2 = tiles[2].getSide(SidePosition.W);
+		assertEquals(s1, s2.getOppositeSide());
+		assertEquals(s1.getOppositeSide(), s2);
+		/* Check entities */
+		Entity e1 = s1.getEntity();
+		Entity e2 = s2.getEntity();
+		assertEquals(e1, e2);
 	}
-
 }
