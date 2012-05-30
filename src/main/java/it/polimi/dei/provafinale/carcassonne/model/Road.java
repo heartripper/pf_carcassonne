ac82cf4e7c35e7aa7cd271b2120ea23a05973612
Chiara <kia90yaya@hotmail.it>
2012-05-30 07:24:16 +0000

    Finito il javadoc.

--- src/main/java/it/polimi/dei/provafinale/carcassonne/model/Road.java
+++ src/main/java/it/polimi/dei/provafinale/carcassonne/model/Road.java
@@ -1,13 +1,12 @@
 package it.polimi.dei.provafinale.carcassonne.model;
 
-
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * The class Road extends Entity and represents one of the possible part the
  * tiles in Carcassonne game.
- *
+ * 
  */
 public class Road extends Entity {
 
@@ -16,6 +15,9 @@ public class Road extends Entity {
 	private EntityType type = EntityType.S;
 	private int id;
 
+	/**
+	 * Road constructor. Creates a new instance of class Road.
+	 */
 	public Road() {
 		id = ++roadCount;
 	}
@@ -30,13 +32,13 @@ public class Road extends Entity {
 		Set<Tile> tiles = new HashSet<Tile>();
 		for (Side s : getMembers()) {
 			Tile card = s.getOwnerCard();
-			if (!tiles.contains(card)){
+			if (!tiles.contains(card)) {
 				tiles.add(card);
 			}
 		}
 		return tiles.size();
 	}
-	
+
 	@Override
 	public String toString() {
 		return type.getRepresentation() + id;
