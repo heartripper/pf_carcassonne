5f8709f5d2fc04843dc801940025f5ee50e4ce02
Chiara <kia90yaya@hotmail.it>
2012-05-30 12:04:37 +0000

    Finito il javadoc.

--- src/main/java/it/polimi/dei/provafinale/carcassonne/controller/server/CarcassonneServer.java
+++ src/main/java/it/polimi/dei/provafinale/carcassonne/controller/server/CarcassonneServer.java
@@ -30,24 +30,21 @@ public class CarcassonneServer {
 	 * Starts the monitors server was instructed to use.
 	 * */
 	public void start() {
-		/* Create matches handler */
+		/* Create matches handler. */
 		MatchesManager manager = new MatchesManager();
 		Thread managerThread = new Thread(manager);
 		managerThread.start();
-
-		/* If requested, start socket request monitor */
+		/* If requested, start socket request monitor. */
 		if (technology == SOCKET || technology == BOTH) {
 			SocketRequestMonitor srm = new SocketRequestMonitor(manager,
 					socketPort);
 			Thread socketThread = new Thread(srm);
 			socketThread.start();
 		}
-
 		/* If requested, start RMI request monitor */
 		if (technology == RMI || technology == BOTH) {
 			RMIRequestMonitor.registerRMIRequestMonitor(manager);
 		}
-
 		/* Waits matches manager thread */
 		try {
 			managerThread.join();
@@ -55,4 +52,5 @@ public class CarcassonneServer {
 			throw new RuntimeException(e);
 		}
 	}
+	
 }
