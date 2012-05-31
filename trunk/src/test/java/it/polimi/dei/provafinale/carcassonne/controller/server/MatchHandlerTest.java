5bec3736d4550a636fe0e00472d9bdeeca2771b6
ThinkyClaire <ThinkyClaire@a852b523-96cf-40f9-898e-13debeb866f8>
2012-05-31 13:46:47 +0000

    Test da finire

---
+++ trunk/src/test/java/it/polimi/dei/provafinale/carcassonne/controller/server/MatchHandlerTest.java
@@ -0,0 +1,61 @@
+package it.polimi.dei.provafinale.carcassonne.controller.server;
+
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+
+import it.polimi.dei.provafinale.carcassonne.PlayerColor;
+import it.polimi.dei.provafinale.carcassonne.controller.Message;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MatchHandlerTest {
+
+	private final int playerNumber = 3;
+
+	private Message msg = new Message(START, null);
+	private Color red;
+	private GameInterface gameInterface;
+	
+	@Before
+	public void setUp(){
+		
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	
+	private class fakeGameInterface implements GameInterface{
+
+		@Override
+		public int askPlayerNumber() {
+			return playerNumber;
+		}
+
+		@Override
+		public Message readFromPlayer(PlayerColor color)
+				throws PlayersDisconnectedException {
+			
+			return null;
+		}
+
+		@Override
+		public void sendPlayer(PlayerColor color, Message msg)
+				throws PlayersDisconnectedException {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void sendAllPlayer(Message msg)
+				throws PlayersDisconnectedException {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+}
