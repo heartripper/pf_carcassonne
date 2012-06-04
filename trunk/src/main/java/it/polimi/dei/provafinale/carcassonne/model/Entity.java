4cdc6423fe187400343e0148f4d1ba241b2a86c6
dariodotcom <dariodotcom@a852b523-96cf-40f9-898e-13debeb866f8>
2012-06-04 08:48:05 +0000

    Move factory method int Entity

--- trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/Entity.java
+++ trunk/src/main/java/it/polimi/dei/provafinale/carcassonne/model/Entity.java
@@ -9,6 +9,28 @@ import java.util.List;
  * Class representing entities (Road and Cities) and shared methods.
  * */
 public abstract class Entity {
+	
+	/*Factory method*/
+	/**
+	 * Creates a new instance of the Entity implementation corresponding to the
+	 * given type.
+	 * 
+	 * @param type
+	 *            - the type of the entity to create
+	 * @return a new instance of a Entity of the given type.
+	 * */
+	public static Entity createByType(EntityType type) {
+		switch (type) {
+		case C:
+			return new City();
+		case S:
+			return new Road();
+		default:
+			return null;
+		}
+
+	}
+	
 	private List<Side> members;
 
 	private boolean completed;
