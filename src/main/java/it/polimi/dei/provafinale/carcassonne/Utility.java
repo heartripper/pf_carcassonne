d798c455f151a0a6b22e0c63b193e704c244df99
Chiara <kia90yaya@hotmail.it>
2012-06-07 11:33:44 +0000

    Tolto il numero di segnalini dalla grafica.

---
+++ src/main/java/it/polimi/dei/provafinale/carcassonne/Utility.java
@@ -0,0 +1,13 @@
+package it.polimi.dei.provafinale.carcassonne;
+
+public final class Utility {
+
+	private Utility(){
+		
+	}
+	
+	public void log(String msg){
+		System.out.println(msg);
+	}
+	
+}
